0156f59ef76f7012f8d3ff138eaa66efe2119e30
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2021-09-19 10:07:10 +0000

    added tee to gtn

--- commands/other/gtn.js
+++ commands/other/gtn.js
@@ -2,14 +2,14 @@ module.exports = {
     name: 'gtn',
     usage: '<Max Number>',
     execute(message, args) {
-        if (message.author.id !== '772524332382945292' && message.author.id !== '598918643727990784' && message.author.id !== '264186213848580096' && message.author.id !== '712316272213491824') return;
-        const number = args[0]
+        if (message.author.id !== '755812617603514439' && message.author.id !== '772524332382945292' && message.author.id !== '598918643727990784' && message.author.id !== '264186213848580096' && message.author.id !== '712316272213491824') return;
+        const number = args[0] 
         let rawGuess = parseInt(number)
         let finalGuess = Math.floor(Math.random() * rawGuess + 1)
         const filter = a => parseInt(a.content) === parseInt(finalGuess)
         message.channel.send(`${message.author} I have dmmed you the right number!`)
         message.author.send('The number to be guessed is **' + finalGuess + '**')
-        console.log(finalGuess)
+
         message.channel.send('Now listening to messages, when someone guesses the right number, the channel will be locked!\n\nGood Luck!')
         message.channel.awaitMessages(filter, {
                 max: 1,
