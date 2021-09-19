ca21da223ba41b23025049a66c0c934bed70f538
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2021-09-19 16:46:54 +0530

    e

--- commands/dev/e.js
+++ commands/dev/e.js
@@ -1,41 +1,36 @@
-const Discord = require('discord.js')
-const {
-  inspect
-} = require('util')
-const mongoose = require('mongoose')
-const grinds = require('../../database/models/grindm')
+const { inspect } = require('util')
+
 module.exports = {
     name: 'eval',
     aliases: ['e'],
-    async execute(message, args, client) {
-        const allowedUser = ['598918643727990784', '455576077629259787', '619339277993639956', '772524332382945292', '450864876416401419']
-        const {
-            member,
-            content
-        } = message
-        if (!allowedUser.includes(message.author.id)) return;
-        const code = message.content.replace(`fh e `, '')
-        try {
-          if(code.includes("token")){
-return message.channel.send("nope")
-}
-            let evaled = eval(code)
-            var embed = new Discord.MessageEmbed()
-                .setColor(0xfcfcfc)
-                .setTitle("Evaluated")
-                .addField("📥 Input", `\`\`\`${code}\`\`\``)
-                .addField("📤 Output", `\`\`\`js\n${inspect(evaled, { depth: 0 })}\`\`\``)
-                .addField("Type", `\`\`\`${typeof(evaled)}\`\`\``)
-                .setTimestamp()
-                .setFooter(`Evaluated in ${client.ws.ping}ms`)
-            message.channel.send(embed)
+    async execute(message, args, client){
+    const allowedUsers = ['598918643727990784', '455576077629259787', '619339277993639956', '772524332382945292', '450864876416401419']
+    let input = args.join(' ');
+    const asynchr = input.includes('return') || input.includes('await');
 
-        } catch (error) {
-            var embed = new Discord.MessageEmbed()
-                .setColor(0xd62424)
-                .setTitle("❌ Error")
-                .setDescription(`${error}`)
-            message.channel.send(embed)
-        }
+    let result, evalTime;
+
+    try {
+      const before = Date.now();
+      result = await eval(asynchr ? `(async()=>{${input}})();` : input) // eslint-disable-line
+      evalTime = Date.now() - before;
+      if (typeof result !== 'string') {
+        result = inspect(result, {
+          depth: +!(inspect(result, { depth: 1 }).length > 1000) // Results in either 0 or 1
+        });
+      }
+      const tokenRegex = new RegExp(client.token, 'gi');
+      result = result.replace(tokenRegex, 'the token was not found, your stupidity was.');
+    } catch (err) {
+      result = err.message;
+    }
+    
+    const embed = new MessageEmbed()
+        .setTitle("EVALED")
+        .addField("📥 Input", `\`\`\`js\n${code}\n\`\`\``)
+        .addField("📤 Output", `\`\`\`js\n${result}\`\`\``)
+        .setFooter(`Evaluated in ${evalTime}ms`)
+
+
     }
 }
