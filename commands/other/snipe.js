3b22c9e590fb50858e7819e0c8327437e1b3d1e8
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2021-09-29 11:01:58 +0530

    snipe

--- commands/other/snipe.js
+++ commands/other/snipe.js
@@ -2,6 +2,17 @@ module.exports = {
     name: 'snipe',
     async execute(message, args, client){
         const sniped = client.snipes.get(message.channel.id)
-        console.log(sniped)
+        if(!sniped || sniped == undefined){
+          message.channel.send("There is nothing to snipe!")
+          return;
+        }
+
+        message.channel.send({embed: {
+          author: {
+              name: sniped.author,
+              url: member.displayAvatarURL({ dynamic: false })
+          },
+          description: sniped.content ? sniped.content : "Probably an image.",
+        }})
     }
 }
--- events/messageDelete.js
+++ events/messageDelete.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'messageDelete',
     once: false,
     execute(message, client){
+      if(message.author.bot) return;
         client.snipes.set(message.channel.id, {
             content: message.content,
             author: message.author.tag,
