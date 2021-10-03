67ec6e93c38af630349d8ff8ca870807bfc75186
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2021-10-03 20:28:19 +0530

    RAID

---
+++ commands/dev/removeraiders.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'removeraiders',
+    aliases: ['deleteraiders', 'fuckraid', 'HELPIMGETTINGRAIDEDAA'],
+    description: 'Kicks all the new members who have joined in the last __x__ minutes/hours.',
+    async execute(message, args, client){
+        if(!['598918643727990784', '619339277993639956'].includes(message.author.id)) return;
+        if(!args[0]) return message.channel.send("You have to provide valid time.")
+        const time = ms(args[0])
+    }
+}
