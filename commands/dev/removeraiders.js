d69e9d204f3fb5709c2a787d25a2041089758841
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2021-10-27 21:19:38 +0530

    fixes

--- commands/dev/removeraiders.js
+++ commands/dev/removeraiders.js
@@ -1,14 +1,39 @@
+const ms = require('ms')
+
 module.exports = {
     name: 'removeraiders',
     aliases: ['deleteraiders', 'fuckraid', 'HELPIMGETTINGRAIDEDAA'],
     description: 'Kicks all the new members who have joined in the last __x__ minutes/hours.',
     async execute(message, args, client){
-        if(!['598918643727990784', '619339277993639956'].includes(message.author.id)) return;
+        if(!['598918643727990784', '619339277993639956', '264186213848580096'].includes(message.author.id)) return;
         if(!args[0]) return message.channel.send("You have to provide valid time.")
         const time = ms(args[0])
         
-        message.guild.members.cache.filter(mem => mem.createdTimestamp - mem.joinedTimestamp < time).forEach(mem => {
-            mem.kick(`Raider. Action requested by ${message.author.tag}`)
+        const size = message.guild.members.cache.filter(mem => message.createdTimestamp - mem.joinedTimestamp < time).size
+
+        message.channel.send(`Are you sure you want to kick **${size.toLocaleString()}** members?\nReply with \`yes\` or \`no\``)
+        const collector = message.channel.createMessageCollector(msg => msg.author.id === message.author.id, { max: 1 })
+
+        collector.on('collect', async message => {
+            if(message.content.toLowerCase() === 'yes'){
+                let kicked = 0;
+                let failed = 0;
+                await message.guild.members.cache.filter(mem => message.createdTimestamp - mem.joinedTimestamp < time).forEach(mem => {
+                    try{
+                        mem.kick(`Raider. Requested by ${message.author.tag}(${message.author.id})`)
+                        kicked++
+                    } catch (e){
+                        failed++
+                    }
+                })
+
+                return message.channel.send(`Done! Kicked a total of ${kicked.toLocaleString()} members.\nFailed to kick ${failed} members.`)
+            } else {
+                message.channel.send('Okay, wont kick those members.')
+            }
+
         })
+
+        
     }
 }
--- commands/fun/fastclick.js
+++ commands/fun/fastclick.js
@@ -10,7 +10,7 @@ module.exports = {
         
         let yesButton = new MessageButton().setStyle("green").setID('yes_fc').setLabel("Accept")
         let noButton = new MessageButton().setStyle("red").setID('no_fc').setLabel("Decline")
-        let row = new MessageActionRow().addComponents([yesButton, noButton])
+        let row = new MessageActionRow().addComponents([noButton, yesButton])
         const confirmation = await message.channel.send({
             embed: {
                 title: 'Confirmation',
@@ -31,7 +31,7 @@ module.exports = {
 
         confirmationCollector.on('collect', async button => {
             if(button.clicker.id !== user2.id){
-               return button.reply.send('This is not for you.')
+               return button.reply.send('This is not for you.', true)
             }
 
             if(button.id === 'yes_fc'){
@@ -46,7 +46,60 @@ module.exports = {
                 timestamp: new Date(),
             }, components: row })
 
-            const mainMessage = await message.channel.send(`Alright! The button will `)
+            const mainMessage = await message.channel.send(`Alright! The button will appear in a few seconds, good luck!`)
+            let mainButton = new MessageButton().setStyle('green').setLabel('This one').setID("correct-fc")
+            let baitButton1 = new MessageButton().setStyle('grey').setLabel('No not this').setID("wrong1-fc")
+            let baitButton2 = new MessageButton().setStyle('grey').setLabel('No not this').setID("wrong2-fc")
+            let baitButton3 = new MessageButton().setStyle('grey').setLabel('No not this').setID("wrong3-fc")
+            let baitButton4 = new MessageButton().setStyle('grey').setLabel('No not this').setID("wrong4-fc")
+            let array = [mainButton, baitButton1, baitButton2, baitButton3, baitButton4].sort(() => Math.random() - 0.5)
+            let mainRow = new MessageActionRow().addComponents(array)
+            await sleep(2500)
+            mainMessage.edit("Click the green one", { components: mainRow })
+
+            const mainCollector = mainMessage.createButtonCollector(b => b, { time: 30000 })
+            mainCollector.on('collect', async button => {
+                if(![user1.id, user2.id].includes(button.clicker.user.id)){
+                    await button.reply.send("This is not for you", true)
+                    return
+                }
+
+                if(button.id !== 'correct-fc'){
+
+                    mainCollector.stop()
+
+                    const loser = button.clicker.user.id
+                    const winner = loser === user1 ? user2 : user1
+                    mainButton = mainButton.setDisabled()
+                    baitButton1 = baitButton1.setDisabled()
+                    baitButton2 = baitButton2.setDisabled()
+                    baitButton3 = baitButton3.setDisabled()
+                    baitButton4 = baitButton4.setDisabled()
+                    array = array
+                    mainRow = new MessageActionRow().addComponents(array)
+                    button.reply.defer()
+                    mainMessage.edit(`:trophy: <@${winner}> has won!`, { components: mainRow })
+                    mainMessage.edit(`:trophy: <@${winner}> won because <@${loser}> clicked the wrong button!`, { components: mainRow })
+                    
+                }
+
+                if(![user1.id, user2.id].includes(button.clicker.user.id)){
+                    await button.reply.send("This is not for you", true)
+                    return
+                }
+                mainCollector.stop()
+                const winner = button.clicker.user.id
+                mainButton = mainButton.setDisabled()
+                baitButton1 = baitButton1.setDisabled()
+                baitButton2 = baitButton2.setDisabled()
+                baitButton3 = baitButton3.setDisabled()
+                baitButton4 = baitButton4.setDisabled()
+                array = array
+                mainRow = new MessageActionRow().addComponents(array)
+                button.reply.defer()
+                mainMessage.edit(`:trophy: <@${winner}> has won!`, { components: mainRow })
+                return;
+            })
             } else {
                 button.reply.defer()
                 yesButton = yesButton.setStyle("grey").setID('yes_fc').setDisabled()
@@ -62,4 +115,8 @@ module.exports = {
             }
         })
     }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
