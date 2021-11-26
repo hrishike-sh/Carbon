6f1aa0140d2a60dee5c0e7d6907ecb3e34351d74
HrishikeshS123 <rushisalokhe456@gmail.com>
2021-11-26 12:42:05 +0530

    should work now.

--- events/tick.js
+++ events/tick.js
@@ -1,6 +1,12 @@
-const { Client, MessageEmbed } = require("discord.js");
+const {
+    Client,
+    MessageEmbed
+} = require("discord.js");
 const giveawayModel = require('../database/models/giveaway')
-const { MessageActionRow, MessageButton } = require('discord-buttons')
+const {
+    MessageActionRow,
+    MessageButton
+} = require('discord-buttons')
 const timerModel = require('../database/models/timer')
 const ms = require('pretty-ms')
 let i = 0;
@@ -26,15 +32,14 @@ module.exports = {
         // Incrementing everything
 
         // Presence
-        const presences = [
-            {
-                name: `${client.users.cache.size.toLocaleString()} fighters!`,
-                type: `WATCHING`
-            },
-            {
-                name: `${client.guilds.cache.size.toLocaleString()} servers!`,
-                type: 'COMPETING'
-            }
+        const presences = [{
+            name: `${client.users.cache.size.toLocaleString()} fighters!`,
+            type: `WATCHING`
+        },
+        {
+            name: `${client.guilds.cache.size.toLocaleString()} servers!`,
+            type: 'COMPETING'
+        }
         ]
         if (presenceCounter1 == 60) {
             presenceCounter1 = 0;
@@ -46,11 +51,17 @@ module.exports = {
                 name: presences[presenceCounter2].name,
                 type: presences[presenceCounter2].type
             })
-        } else if (gawCounter1 == 5) {
+        }
+        // Presence
+
+        // Giveaways
+        if (gawCounter1 == 5) {
             gawCounter1 = 0;
 
             const gaws = await giveawayModel.find({
-                endsAt: { $lte: new Date().getTime() },
+                endsAt: {
+                    $lte: new Date().getTime()
+                },
                 hasEnded: false
             });
 
@@ -58,75 +69,79 @@ module.exports = {
                 setTimeout(() => {
                     client.emit('tick')
                 }, 1000)
-                return;
+            } else {
+
+                for (const giveaway of gaws) {
+
+                    giveaway.hasEnded = true;
+                    giveaway.save()
+
+                    const channel = await client.channels.cache.get(`${giveaway.channelId}`)
+                    if (!channel) {
+                        setTimeout(() => {
+                            client.emit('tick')
+                        }, 1000)
+                    } else {
+                        const message = await channel.messages.fetch(`${giveaway.messageId}`)
+                        if (!message) {
+                            setTimeout(() => {
+                                client.emit('tick')
+                            }, 1000)
+                        } else {
+                            const winner = `<@${giveaway.entries[Math.floor(Math.random() * giveaway.entries.length)]}>`
+
+                            await message.edit("This giveaway has ended.", {
+                                embed: {
+                                    title: giveaway.prize || '',
+                                    description: `Winner: ${winner}\nHosted By: <@${giveaway.hosterId}>`,
+                                    color: 'black',
+                                    footer: {
+                                        text: `Winners: ${giveaway.winners}`,
+                                    },
+                                    timestamp: new Date(),
+                                },
+                                components: new MessageActionRow().addComponents([new MessageButton().setStyle("green").setID('whydodisabledbuttonsneedanid').setLabel("Enter").setDisabled(), new MessageButton().setStyle("grey").setID("giveaway-info").setLabel("View Info")])
+                            })
+
+                            await channel.send(`The giveaway for **${giveaway.prize}** has ended and the winner is ${winner}!`, {
+                                embed: {
+                                    title: 'Giveaway Info',
+                                    description: `Entries: **${giveaway.entries.length.toLocaleString()}**\nChances of winning: **${(1 / giveaway.entries.length * 100).toFixed(3)}%**`,
+                                    footer: {
+                                        text: "Congrats!"
+                                    },
+                                    timestamp: new Date()
+                                }
+                            })
+
+                            client.users.cache.get(`${giveaway.hosterId}`).send({
+                                embed: {
+                                    title: "Giveaway Result",
+                                    description: `The giveaway you hosted has ended!`,
+                                    fields: [{
+                                        name: "Winner",
+                                        value: winner,
+                                    },
+                                    {
+                                        name: 'Link',
+                                        value: `[Jump](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})`
+                                    }
+                                    ]
+                                }
+                            })
+                        }
+
+                    }
+
+
+                }
             }
 
-            for (const giveaway of gaws) {
+        }
+        // Giveaways
 
-                giveaway.hasEnded = true;
-                giveaway.save()
-
-                const channel = await client.channels.cache.get(`${giveaway.channelId}`)
-                if (!channel) {
-                    setTimeout(() => {
-                        client.emit('tick')
-                    }, 1000)
-                    return
-                }
-
-                const message = await channel.messages.fetch(`${giveaway.messageId}`)
-                if (!message) {
-                    setTimeout(() => {
-                        client.emit('tick')
-                    }, 1000)
-                    return
-                }
-
-                const winner = `<@${giveaway.entries[Math.floor(Math.random() * giveaway.entries.length)]}>`
-
-                await message.edit("This giveaway has ended.", {
-                    embed: {
-                        title: giveaway.prize || '',
-                        description: `Winner: ${winner}\nHosted By: <@${giveaway.hosterId}>`,
-                        color: 'black',
-                        footer: {
-                            text: `Winners: ${giveaway.winners}`,
-                        },
-                        timestamp: new Date(),
-                    },
-                    components: new MessageActionRow().addComponents([new MessageButton().setStyle("green").setID('whydodisabledbuttonsneedanid').setLabel("Enter").setDisabled(), new MessageButton().setStyle("grey").setID("giveaway-info").setLabel("View Info")])
-                })
-
-                await channel.send(`The giveaway for **${giveaway.prize}** has ended and the winner is ${winner}!`, {
-                    embed: {
-                        title: 'Giveaway Info',
-                        description: `Entries: **${giveaway.entries.length.toLocaleString()}**\nChances of winning: **${(1 / giveaway.entries.length * 100).toFixed(3)}%**`,
-                        footer: {
-                            text: "Congrats!"
-                        },
-                        timestamp: new Date()
-                    }
-                })
-
-                client.users.cache.get(`${giveaway.hosterId}`).send({
-                    embed: {
-                        title: "Giveaway Result",
-                        description: `The giveaway you hosted has ended!`,
-                        fields: [
-                            {
-                                name: "Winner",
-                                value: winner,
-                            },
-                            {
-                                name: 'Link',
-                                value: `[Jump](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})`
-                            }
-                        ]
-                    }
-                })
-
-            }
-        } else if (randomColorCounter == 600) {
+        // Random Color
+        if (randomColorCounter == 600) {
             randomColorCounter = 0;
 
             const random_hex_color_code = () => {
@@ -137,11 +152,17 @@ module.exports = {
 
             client.guilds.cache.get("817734579246071849").roles.cache.get('900274849153445918').setColor(random_hex_color_code())
 
-        } else if (timerCounter == 10) {
+        }
+        // Random Color
+
+        // Timer
+        if (timerCounter == 10) {
             timerCounter = 0
 
             const timers = timerModel.find({
-                time: { $gte: new Date().getTime() },
+                time: {
+                    $gte: new Date().getTime()
+                },
                 ended: false,
             })
 
@@ -149,41 +170,44 @@ module.exports = {
                 setTimeout(() => {
                     client.emit('tick')
                 }, 1000)
-                return;
-            }
+            } else {
 
-            console.log(timers)
 
-            for (const timer of timers) {
-                const time = timer.time - new Date().getTime()
-                if (time < 0) {
-                    timer.ended = true
-                    timer.save()
+                console.log(timers)
+
+                for (const timer of timers) {
+                    const time = timer.time - new Date().getTime()
+                    if (time < 0) {
+                        timer.ended = true
+                        timer.save()
+                    }
+
+                    const channel = client.channels.cache.get(timer.channelId)
+                    if (!channel) {
+                        setTimeout(() => {
+                            client.emit('tick')
+                        }, 1000)
+                    } else {
+
+
+                        const message = await channel.messages.fetch(timer.messageId)
+                        if (!message) {
+                            setTimeout(() => {
+                                client.emit('tick')
+                            }, 1000)
+                        } else {
+
+
+                            message.edit(
+                                new MessageEmbed()
+                                    .setAuthor(timer.member.displayName, timer.member.user.displayAvatarURL())
+                                    .setTitle(timer.reason)
+                                    .setColor("RANDOM")
+                                    .setDescription(`${ms(time, { verbose: true })}`)
+                            )
+                        }
+                    }
                 }
-
-                const channel = client.channels.cache.get(timer.channelId)
-                if (!channel) {
-                    setTimeout(() => {
-                        client.emit('tick')
-                    }, 1000)
-                    return
-                }
-
-                const message = await channel.messages.fetch(timer.messageId)
-                if (!message) {
-                    setTimeout(() => {
-                        client.emit('tick')
-                    }, 1000)
-                    return
-                }
-
-                message.edit(
-                    new MessageEmbed()
-                        .setAuthor(timer.member.displayName, timer.member.user.displayAvatarURL())
-                        .setTitle(timer.reason)
-                        .setColor("RANDOM")
-                        .setDescription(`${ms(time, { verbose: true })}`)
-                )
             }
         }
         // Timer
