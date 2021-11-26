0f79a4ad1a08085ad3cafbbbce56ac0f207b9f0b
HrishikeshS123 <rushisalokhe456@gmail.com>
2021-11-26 18:55:56 +0530

    bro im dumb

--- events/tick.js
+++ events/tick.js
@@ -193,7 +193,7 @@ module.exports = {
 
                             message.edit(
                                 new MessageEmbed()
-                                    .setAuthor(timer.member.username, timer.member.user.displayAvatarURL())
+                                    .setAuthor(client.users.cache.get(timer.member).displayAvatarURL(), timer.member.user.displayAvatarURL())
                                     .setTitle(timer.reason)
                                     .setColor("RANDOM")
                                     .setDescription(`${ms(time, { verbose: true })}`)
