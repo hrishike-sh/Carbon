2e84cb0e876b01830696b1d4f48a1931fa661f10
HrishikeshS123 <rushisalokhe456@gmail.com>
2021-11-28 09:26:18 +0530

    chess

--- commands/fun/discord-together.js
+++ commands/fun/discord-together.js
@@ -5,7 +5,7 @@ module.exports = {
     async execute(message, args, client) {
         if (message.author.id !== '598918643727990784') return message.channel.send(`You are not hrish go away.`)
 
-        client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(code => {
+        client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'chess').then(code => {
             return message.channel.send(code.code)
         })
     }
