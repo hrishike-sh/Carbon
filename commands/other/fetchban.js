744f3ddc0782b8541416d93dd24de56d2e870970
Dauntless7 <nicolai.8293@gmail.com>
2021-12-09 17:09:58 -0400

    restrict fetchban to mod/admin

--- commands/other/fetchban.js
+++ commands/other/fetchban.js
@@ -1,26 +1,44 @@
 module.exports = {
-  name: 'fetchban',
-  aliases: ['fetchbans', 'baninfo'],
+  name: "fetchban",
+  aliases: ["fetchbans", "baninfo"],
   args: true,
-  usage: '<id>',
-  description: 'Check ban info about a certain user.',
-  async execute(message, args){
-    const id = args[0]
-    let banInfo = await message.guild.fetchBan(id).catch(e => {return message.channel.send("Either the user is not banned or the user id provided is not valid.")})
+  usage: "<id>",
+  description: "Check ban info about a certain user.",
+  async execute(message, args) {
+    const id = args[0];
+    // daunt: move to config
+    const roles = {
+      mod: "824348974449819658",
+      admin: "824539655134773269",
+    };
 
-    if(!banInfo) return;
+    if (
+      !message.member.roles.cache.some(
+        (r) => r === roles.mod || r === roles.admin
+      )
+    ) {
+      return;
+    }
+    let banInfo = await message.guild.fetchBan(id).catch(() => {
+      return message.channel.send(
+        "Either the user is not banned or the user ID provided is not valid."
+      );
+    });
+
+    if (!banInfo) return;
 
     message.channel.send({
       embed: {
-        title: 'Ban info',
-        description: `User: \`${banInfo.user.username + '#' + banInfo.user.discriminator}\`(${id})`,
+        description: `User: \`${
+          banInfo.user.username + "#" + banInfo.user.discriminator
+        }\`(${id})`,
         fields: [
           {
             name: "Reason",
-            value: banInfo.reason
-          }
-        ]
-      }
-    })
-  }
-}
+            value: banInfo.reason,
+          },
+        ],
+      },
+    });
+  },
+};
