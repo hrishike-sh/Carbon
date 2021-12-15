6aad7b044d255ec28056618f0a64b9bc0864dc65
Dauntless7 <nicolai.8293@gmail.com>
2021-12-15 15:26:36 -0400

    prettier file + fix grammar

--- commands/moderation/lockdown.js
+++ commands/moderation/lockdown.js
@@ -1,68 +1,80 @@
-
 const db = require('../../database/models/settingsSchema')
 const { MessageButton, MessageActionRow } = require('discord-buttons')
 
 module.exports = {
     name: 'lockdown',
-    async execute(message, args){
-        if(!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(`You must have the ADMINISTATOR permission to run this command`)
+    async execute(message) {
+        if (!message.member.permissions.has('ADMINISTRATOR'))
+            return message.channel.send(
+                `You must have the ADMINISTATOR permission to run this command`
+            )
 
         const server = await db.findOne({ guildID: message.guild.id })
 
-        if(!server || !server.lockdownSet.channels) return message.channel.send(`This server has not yet set the channels that are supposed to be locked down.\nCheck \`fh lds\` for more info.`)
-        
-        const yesbut = new MessageButton().setLabel("Yes").setStyle("green").setID("lock-yes")
-        const nobut = new MessageButton().setLabel("No").setStyle("red").setID("lock-no")
+        if (!server || !server.lockdownSet.channels)
+            return message.channel.send(
+                `This server has not yet set the channels that are supposed to be locked down.\nCheck \`fh lds\` for more info.`
+            )
+
+        const yesbut = new MessageButton()
+            .setLabel('Yes')
+            .setStyle('green')
+            .setID('lock-yes')
+        const nobut = new MessageButton()
+            .setLabel('No')
+            .setStyle('red')
+            .setID('lock-no')
         const row1 = new MessageActionRow().addComponents([yesbut, nobut])
 
-        const confirm = await message.channel.send(`Are you sure you want to lockdown?`, { components: row1 })
+        const confirm = await message.channel.send(
+            `Are you sure you want to lockdown?`,
+            { components: row1 }
+        )
 
-        const collectorC = confirm.createButtonCollector(b => b, { time: 30000, max: 1 })
+        const collectorC = confirm.createButtonCollector((b) => b, {
+            time: 30000,
+            max: 1,
+        })
 
-        collectorC.on('collect', async button => {
-            if(button.clicker.user.id !== message.author.id){
+        collectorC.on('collect', async (button) => {
+            if (button.clicker.user.id !== message.author.id) {
                 button.reply.send(`This is not for you`, true)
                 return
             }
-            
+
             const id = button.id
-            
-            if(id === 'lock-no'){
+
+            if (id === 'lock-no') {
                 confirm.delete()
                 return message.channel.send(`I guess not.`)
-            } else if (id === 'lock-yes'){
+            } else if (id === 'lock-yes') {
                 const channels = server.lockdownSet.channels
 
-                for(const channel of channels){
-                    const toLockChannel = message.guild.channels.cache.get(channel)
+                for (const channel of channels) {
+                    const toLockChannel =
+                        message.guild.channels.cache.get(channel)
 
-                    toLockChannel.updateOverwrite(message.channel.guild.roles.everyone, {
-                        SEND_MESSAGES: false
-                    })
+                    toLockChannel.updateOverwrite(
+                        message.channel.guild.roles.everyone,
+                        {
+                            SEND_MESSAGES: false,
+                        }
+                    )
 
                     toLockChannel.send({
                         embed: {
                             title: ':lock: **SERVER LOCKDOWN**',
-                            description: server.lockdownSet.message || "The server is under lockdown.",
-                            color: "RED",
-                            timestamp: new Date()
-                        }
+                            description:
+                                server.lockdownSet.message ||
+                                'The server is currently locked.',
+                            color: 'RED',
+                            timestamp: new Date(),
+                        },
                     })
                 }
 
-                message.channel.send(`Done, the server is now lock downed!`)
-
-                
+                message.channel.send('Done, the server is now on lockdown!')
             }
-
-
-
         })
-
-    }
+    },
 }
-
-
-
-
-
