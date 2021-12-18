b60690a35b7b6b23e0353ec926637b0fffc1dbac
HrishikeshS123 <rushisalokhe456@gmail.com>
2021-12-18 19:37:48 +0530

    fancy help command

--- commands/other/help.js
+++ commands/other/help.js
@@ -1,50 +1,58 @@
 const prefix = 'fh '
-
+const { Message, Client } = require('discord.js')
 module.exports = {
     name: 'help',
     description: 'Please send help',
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
     async execute(message, args, client) {
+        /**commands.map(command => command.name).join(', ') */
         const data = []
-        const {
-            commands
-        } = client
+        const { commands } = client
 
         if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+            const commandsMap = commands
+                .map((command) => `\`${command.name}\``)
+                .join(', ')
 
-            return message.author.send(data, {
-                    split: true
-                })
-                .then(() => {
-                    if (message.channel.type === 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-                });
+            message.react('📨')
+            return message.author.send(
+                new MessageEmbed()
+                    .setTitle('Help Command')
+                    .setDescription(commandsMap)
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .setColor('GREEN')
+                    .setFooter(
+                        'Type fh help [command name] for further info about the command.'
+                    )
+            )
         }
 
-        const name = args[0].toLowerCase();
-        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+        const name = args[0].toLowerCase()
+        const command =
+            commands.get(name) ||
+            commands.find((c) => c.aliases && c.aliases.includes(name))
 
         if (!command) {
-            return message.reply('that\'s not a valid command!');
+            return message.reply("that's not a valid command!")
         }
 
-        data.push(`**Name:** ${command.name}`);
+        data.push(`**Name:** ${command.name}`)
 
-        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        if (command.aliases)
+            data.push(`**Aliases:** ${command.aliases.join(', ')}`)
+        if (command.description)
+            data.push(`**Description:** ${command.description}`)
+        if (command.usage)
+            data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`)
 
-        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`)
 
         message.channel.send(data, {
-            split: true
-        });
-
-    }
+            split: true,
+        })
+    },
 }
