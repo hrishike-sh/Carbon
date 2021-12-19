0f5a62dc99abb1b3f4d8adebcd0be51c78c70081
HrishikeshS123 <rushisalokhe456@gmail.com>
2021-12-19 15:02:42 +0530

    pretty file + category

--- commands/christmas/presents.js
+++ commands/christmas/presents.js
@@ -1,29 +1,41 @@
 const db = require('../../database/models/presentSchema')
-const { Client, Message, MessageEmbed } = require('discord.js');
+const { Client, Message, MessageEmbed } = require('discord.js')
 
 module.exports = {
     name: 'presents',
     fhOnly: false,
     disabledChannels: [],
-    /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
+    category: {
+        name: 'christmas',
+        displayName: 'Christmas Special',
+    },
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
      */
     async execute(message, args, client) {
-        const userId = message.mentions.users.size > 0 ? message.mentions.users.first().id : message.member.id
+        const userId =
+            message.mentions.users.size > 0
+                ? message.mentions.users.first().id
+                : message.member.id
         const user = await db.findOne({ userId })
-        const lb = await db.find({}).sort({
-            presents: -1
-        }).limit(1)
+        const lb = await db
+            .find({})
+            .sort({
+                presents: -1,
+            })
+            .limit(1)
         const presents = user ? user.presents : 0
 
         console.log(lb)
         message.channel.send(
             new MessageEmbed()
-                .setTitle("Presents")
-                .setDescription(`**Your presents:** ${presents.toLocaleString()}\n**Leaderboard top:** ${lb[0].presents.toLocaleString()}`)
-                .setColor("WHITE")
+                .setTitle('Presents')
+                .setDescription(
+                    `**Your presents:** ${presents.toLocaleString()}\n**Leaderboard top:** ${lb[0].presents.toLocaleString()}`
+                )
+                .setColor('WHITE')
         )
-    }
+    },
 }
