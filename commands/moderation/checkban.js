6003f1e083c7651a407fa1ca8699ff8a5d7ce7b2
HrishikeshS123 <rushisalokhe456@gmail.com>
2021-12-26 15:11:29 +0530

    fetchban fix

--- commands/moderation/checkban.js
+++ commands/moderation/checkban.js
@@ -1,33 +1,55 @@
+const { Message, Client } = require('discord.js')
 module.exports = {
     name: 'checkban',
     aliases: ['cb', 'fhml'],
+    /**
+     *
+     * @param {Message} message
+     * @param {String[]} args
+     * @param {Client} client
+     * @returns
+     */
     async execute(message, args, client) {
-        const fh = client.guilds.cache.get("824294231447044197")
-        if (message.guild.id !== '845215901657071647') return;
-        if (!message.member.roles.cache.has("848576301182877727") && !message.member.roles.cache.has("848580138970251314")) return;
-        if (!args[0]) return message.channel.send("Please provide the user id.")
+        const fh = client.guilds.cache.get('824294231447044197')
+        if (message.guild.id !== '845215901657071647') return
+        if (
+            !message.member.roles.cache.has('848576301182877727') &&
+            !message.member.roles.cache.has('848580138970251314')
+        )
+            return
+        if (!args[0]) return message.channel.send('Please provide the user id.')
 
-        const id = message.mentions.users.size > 0 ? message.mentions.users.first().id : args[0]
+        const id =
+            message.mentions.users.size > 0
+                ? message.mentions.users.first().id
+                : args[0]
 
-        let ban = await fh.fetchBan(id).catch(e => {
-            message.channel.send(`Either the provided user is not banned or the user id is invalid.`)
-            return;
-        }) || null
+        let ban =
+            (await fh.bans.fetch(id).catch((e) => {
+                message.channel.send(
+                    `Either the provided user is not banned or the user id is invalid.`
+                )
+                return
+            })) || null
 
-        if (!ban) return;
+        if (!ban) return
 
         message.channel.send({
-            embed: {
-                title: 'Ban Info',
-                color: 'RED',
-                description: `User: \`${ban.user ? ban.user.tag : "Unknown"}\`(\`${ban.user.id}\`)`,
-                fields: [
-                    {
-                        name: 'Reason',
-                        value: ban.reason
-                    }
-                ]
-            }
+            embeds: [
+                {
+                    title: 'Ban Info',
+                    color: 'RED',
+                    description: `User: \`${
+                        ban.user ? ban.user.tag : 'Unknown'
+                    }\`(\`${ban.user.id}\`)`,
+                    fields: [
+                        {
+                            name: 'Reason',
+                            value: ban.reason,
+                        },
+                    ],
+                },
+            ],
         })
-    }
+    },
 }
