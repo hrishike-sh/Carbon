1d5d7b1768d99dea43915234e05c1a81facbf248
Dauntless7 <nicolai.8293@gmail.com>
2021-12-31 15:17:59 -0400

    changes

--- commands/other/gtn.js
+++ commands/other/gtn.js
@@ -6,10 +6,8 @@ module.exports = {
     execute(message, args) {
         if (
             message.author.id !== '450864876416401419' &&
-            message.author.id !== '755812617603514439' &&
             message.author.id !== '772524332382945292' &&
             message.author.id !== '598918643727990784' &&
-            message.author.id !== '264186213848580096' &&
             message.author.id !== '712316272213491824'
         )
             return
