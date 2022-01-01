d1c08c8f05216c0d494373812b5be38f14c44719
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-01 11:47:22 +0530

    more gstart options

--- slashcommands/gstart.js
+++ slashcommands/gstart.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const { ChannelType } = require('discord-api-types')
 const { CommandInteraction } = require('discord.js')
 
 module.exports = {
@@ -22,7 +23,33 @@ module.exports = {
                 .setName('prize')
                 .setDescription('Specify the prize for the giveaway')
                 .setRequired(true)
+        })
+        .addChannelOption((option) => {
+            return option
+                .setName('channel')
+                .setDescription(
+                    'Mention the channel you want the giveaway to be in.'
+                )
+                .setRequired(true)
+                .addChannelType(ChannelType.GuildText)
+        })
+        .addStringOption((option) => {
+            return option
+                .setRequired(false)
+                .setName('role_requirement')
+                .setDescription(
+                    "Add role requirements to the giveaway, add multiple by seperating roles ids with ' '"
+                )
+        })
+        .addUserOption((option) => {
+            return option
+                .setRequired(false)
+                .setName('donator')
+                .setDescription(
+                    'The person who is donating towards the giveaway'
+                )
         }),
+
     /**
      *
      * @param {CommandInteraction} interaction
@@ -32,10 +59,17 @@ module.exports = {
             prize: interaction.options.getString('prize'),
             winners: interaction.options.getNumber('winners'),
             time: interaction.options.getString('time'),
+            channel: interaction.options.getChannel('channel'),
+            req: interaction.options.getString('role_requirement'),
+            donor: interaction.options.getUser('donator'),
         }
 
         interaction.reply({
-            content: `**Prize**: ${data.prize}\n**Winners**: ${data.winners}\n**Time**: ${data.time}`,
+            content: `**Prize**: ${data.prize}\n**Winners**: ${
+                data.winners
+            }\n**Time**: ${data.time}\n**Roles**: <@${
+                data.req
+            }>\n**Donator**: ${data.donor.toString()}\n**Channel**: ${data.channel.toString()}`,
         })
     },
 }
