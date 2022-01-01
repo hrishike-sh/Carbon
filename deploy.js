c156992d3d25e34e8f2043e3b886831ccb13eb5e
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-01 10:51:21 +0530

    log to check slash command

--- deploy.js
+++ deploy.js
@@ -12,8 +12,18 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON())
 }
 
-const rest = new REST({ version: '9' }).setToken(process.env.token)
+const rest = new REST({ version: '9' }).setToken(process.env.token)(
+    async () => {
+        try {
+            console.log('Started refreshing application (/) commands.')
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Registered slash commands.'))
-    .catch(console.error)
+            await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+                body: commands,
+            })
+
+            console.log('Successfully reloaded application (/) commands.')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)()
