9fb7e4a3301e4ce68bccd86454ef1c2d18a5f711
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-08 15:21:48 +0530

    timeout done

--- slashcommands/timeout.js
+++ slashcommands/timeout.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { CommandInteraction } = require('discord.js')
-
+const ms = require('ms')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('timeout')
@@ -9,6 +9,14 @@ module.exports = {
             return amogus
                 .setName('user')
                 .setDescription('The user you want to timeout.')
+                .setRequired(true)
+        })
+        .addStringOption((red) => {
+            return red
+                .setName('time')
+                .setDescription(
+                    'Specify the amount of time the user should be timed out.'
+                )
                 .setRequired(true)
         })
         .addStringOption((sus) => {
@@ -27,9 +35,42 @@ module.exports = {
      * @param {CommandInteraction} interaction
      */
     async execute(interaction) {
-        await interaction.reply({
-            content: 'HmMmMM',
-            ephemeral: true,
+        const data = {
+            user: interaction.options.getUser('user'),
+            time: interaction.options.getString('time'),
+            reason: interaction.options.getString('reason'),
+        }
+
+        const target = await interaction.guild.members.fetch({
+            user: data.user,
         })
+        if (!target) {
+            return interaction.reply({
+                content: `I could not find that user in this guild.`,
+                ephemeral: true,
+            })
+        }
+
+        const time = ms(data.time)
+        if (isNaN(time)) {
+            return interaction.reply({
+                content: `Please provide valid time, could not parse "${data.time}" properly.`,
+                ephemeral: true,
+            })
+        }
+
+        try {
+            await target.timeout(time, data.reason.toString())
+            return interaction.reply({
+                content: `${target.toString()} has been timed out for ${ms(
+                    time,
+                    { long: true }
+                )}`,
+            })
+        } catch (e) {
+            return interaction.reply({
+                content: `Could not timeout the user.\nError: ${e.message}`,
+            })
+        }
     },
 }
