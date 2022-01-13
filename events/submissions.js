c39b4048e9d5a6812938ed1b7db9de4fd6714ed6
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-13 11:59:23 +0530

    fixed disabled buttons

--- events/submissions.js
+++ events/submissions.js
@@ -26,36 +26,51 @@ module.exports = {
         )
         const embed = button.message.embeds[0]
         if (id === 'accept-submit') {
-        } else {
             const comp = button.message.components
             comp.forEach((sus) => {
                 sus.setDisabled()
             })
+            embed.setColor('GREEN')
+        } else {
             embed.setColor('RED')
-            button.message.edit({
-                embeds: [
-                    {
-                        title: 'Denied',
-                        description: `Denied by: **${
-                            button.user.tag
-                        }**\nTime: <t:${(new Date.getTime() / 1000).toFixed(
-                            0
-                        )}:R>\n\nThe user was DM'd about this.`,
-                    },
-                    embed,
-                ],
-                components: [comp],
-            })
-            ;(await user).send({
-                embeds: [
-                    {
-                        title: 'Submission update',
-                        description: `Your submission for the event was __denied__.\n\nDenied by: ${button.user.tag}`,
-                        timestamp: new Date(),
-                        color: 'RED',
-                    },
-                ],
-            })
+            button.message
+                .edit({
+                    embeds: [
+                        {
+                            title: 'Denied',
+                            description: `Denied by: **${
+                                button.user.tag
+                            }**\nTime: <t:${(new Date.getTime() / 1000).toFixed(
+                                0
+                            )}:R>\n\nThe user was DM'd about this.`,
+                        },
+                        embed,
+                    ],
+                    components: [
+                        new MessageActionRow().addComponents([
+                            new MessageButton()
+                                .setLabel('Accept')
+                                .setCustomId('accept-submit')
+                                .setStyle('SUCCESS')
+                                .setDisabled(),
+                            new MessageButton()
+                                .setStyle('DANGER')
+                                .setLabel('Deny')
+                                .setCustomId('deny-submit')
+                                .setDisabled(),
+                        ]),
+                    ],
+                })(await user)
+                .send({
+                    embeds: [
+                        {
+                            title: 'Submission update',
+                            description: `Your submission for the event was __denied__.\n\nDenied by: ${button.user.tag}`,
+                            timestamp: new Date(),
+                            color: 'RED',
+                        },
+                    ],
+                })
         }
     },
 }
