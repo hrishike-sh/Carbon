ce26926ab37fe48ff5519fe083419d23223ca1d5
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-13 10:19:38 +0530

    submit thing

--- slashcommands/submit.js
+++ slashcommands/submit.js
@@ -1,5 +1,11 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { CommandInteraction, MessageEmbed } = require('discord.js')
+const {
+    CommandInteraction,
+    MessageEmbed,
+    Message,
+    MessageActionRow,
+    MessageButton,
+} = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('submit')
@@ -24,17 +30,60 @@ module.exports = {
                 content: 'No valid link was provided.',
             })
         }
-
-        await interaction.reply({
-            content: 'Test Submission',
+        const row = new MessageActionRow().addComponents([
+            new MessageButton()
+                .setLabel('Confirm')
+                .setStyle('SUCCESS')
+                .setCustomId('yes-submit'),
+            new MessageButton()
+                .setLabel('No, go back.')
+                .setStyle('DANGER')
+                .setCustomId('no-submit'),
+        ])
+        const message = await interaction.reply({
+            content: 'Are you sure you want to submit this?',
             embeds: [
                 {
-                    title: 'This is how your submission will look.',
+                    title: `Submission by ${interaction.user.tag}`,
+                    footer: {
+                        text: 'Use the buttons to proceed.',
+                    },
                     image: {
                         url: link,
                     },
                 },
             ],
+            components: [row],
+        })
+
+        const confirmationCollector = message.createMessageComponentCollector({
+            time: 30000,
+        })
+
+        confirmationCollector.on('collect', async (button) => {
+            if (!button.isButton()) return // this isnt even required but ok
+
+            const id = button.customId
+            if (button.user.id !== interaction.user.id) {
+                return button.reply({
+                    content: 'This is not for you.',
+                    ephemeral: true,
+                })
+            }
+
+            row.components.forEach((but) => {
+                but.setDisabled()
+            })
+
+            if (id === 'yes-submit') {
+                return button.reply({
+                    content: 'That worked lol, but it still didnt do stuff',
+                })
+            } else {
+                return button.reply({
+                    content: 'Okay, not doing that.',
+                })
+            }
         })
     },
 }
