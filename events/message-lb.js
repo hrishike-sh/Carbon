7d0c0b2a2499f1411c195f1fecd7a249b318de56
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-02-14 21:53:33 +0530

    lastping

--- events/message-lb.js
+++ events/message-lb.js
@@ -1,28 +1,43 @@
-
-const { Message, Client, Collection } = require("discord.js")
-
+const { Message, Client, Collection } = require('discord.js')
+const pings = require('../database/models/ping')
 module.exports = {
-    name: "messageCreate",
+    name: 'messageCreate',
     /**
-     * 
-     * @param {Message} message 
-     * @param {Client} client 
+     *
+     * @param {Message} message
+     * @param {Client} client
      */
-    async execute(message, client){
-        if(message.guild.id !== '824294231447044197') return;
-        if(message.author.bot) return;
-        
-        const messages = client.db.messages
+    async execute(message, client) {
+        if (message.author.bot) return
+        if (message.guild.id !== '824294231447044197') return
 
-        if(!messages.has(message.author.id)){
-            messages.set(message.author.id, {
-                messages: 0
-            })
-        }
-        
-        const user = messages.get(message.author.id)
-        user.messages++
+        if (message.mentions.members.size < 1) return
+        message.mentions.members.forEach((member) => {
+            if (
+                member.roles.cache.hasAny([
+                    '826197829126979635',
+                    '824687526396297226',
+                    '825965323500126208',
+                    '828048225096826890',
+                ])
+            ) {
+                let user = await pings.findOne({ userId: member.id })
+                if (!user) {
+                    user = new pings({
+                        userId: member.id,
+                        pings: [],
+                    })
+                }
 
-        return;
-    }
+                user.pings.push({
+                    when: new Date().getTime(),
+                    content: message.content,
+                    message_link: `https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`,
+                    author: message.author,
+                    channel: message.channel.id,
+                })
+                user.save()
+            } else return
+        })
+    },
 }
