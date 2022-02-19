4849dbbfd8764df904169ad056a5532a48232d87
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-02-19 18:44:10 +0530

    suggest command

---
+++ slashcommands/suggest.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { CommandInteraction, Client, MessageEmbed } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName(`suggest`)
+        .setDescription('Suggest something you would like to see in this bot!')
+        .addStringOption((opt) => {
+            return opt
+                .setName('suggestion')
+                .setRequired(true)
+                .setDescription('Your suggestion.')
+        })
+        .addBooleanOption((opt) => {
+            return opt
+                .setName('anonymous')
+                .setDescription(
+                    'Do you want your suggestion to be anonymous? Default: false'
+                )
+                .setRequired(false)
+        }),
+    /**
+     *
+     * @param {CommandInteraction} interaction
+     * @param {Client} client
+     */
+    async execute(interaction, client) {
+        const data = {
+            suggestion: interaction.options.getString('suggestion'),
+            a: interaction.options.getBoolean('anonymous') || false,
+        }
+
+        const suggestChannel = client.channels.cache.get(
+            client.config.suggestChannel
+        )
+        const suggestEmbed = new MessageEmbed()
+            .setDescription(data.suggestion)
+            .setTimestamp()
+            .setColor('GREEN')
+
+        if (data.a) {
+            suggestEmbed.setAuthor({
+                name: `Anonymous' suggestion`,
+            })
+        } else {
+            suggestEmbed
+                .setAuthor({
+                    name: `${interaction.user.tag}'s suggestion:`,
+                    iconURL: interaction.user.displayAvatarURL(),
+                })
+                .setFooter({
+                    text: interaction.user.id.toString(),
+                })
+        }
+
+        suggestChannel.send({
+            embeds: [],
+        })
+    },
+}
