ecb9ddebbeb10d6e7aceed0cddfc568122481a85
Fellintron <fellintron@gmail.com>
2022-02-20 21:21:48 +0530

    every 12h status update

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ client.on('ready', async () => {
     console.log('Logged in.')
     client.emit('tick')
     client.user.setActivity({
-        name: 'https://imgur.com/wYkzufz',
+        name: 'ping hrishikesh',
         type: 'LISTENING',
     })
     client.db.fighthub = client.guilds.cache.get(config.guildId)
