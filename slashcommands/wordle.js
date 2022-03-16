9f2c3f197032409a86a6166a8790ecbd72316992
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-03-16 18:32:22 +0530

    forgot sleep time 💀💀

--- slashcommands/wordle.js
+++ slashcommands/wordle.js
@@ -116,7 +116,7 @@ module.exports = {
                 return msg.reply('The word must only contain alphabets.')
 
             for (let i = 0; i < msg.content.length; i++) {
-                await client.functions.sleep()
+                await client.functions.sleep(500)
                 Game.components[currentLine].components[i].label =
                     msg.content[i].toUpperCase()
                 Game.components[currentLine].components[i].emoji = null
