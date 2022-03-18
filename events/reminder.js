a066aa660761f96f2a444be3e21de800e010f0f5
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-03-18 21:48:08 +0530

    [SHUT UP]("https://i.imgur.com/33UmOmL.png")

--- events/reminder.js
+++ events/reminder.js
@@ -1,56 +1,56 @@
-const { Client, MessageEmbed } = require('discord.js')
+// const { Client, MessageEmbed } = require('discord.js')
 
-module.exports = {
-    name: 'tick',
-    /**
-     * @param {Client} client
-     */
-    async execute(client) {
-        const reminders = client.db.reminders
+// module.exports = {
+//     name: 'tick',
+//     /**
+//      * @param {Client} client
+//      */
+//     async execute(client) {
+//         const reminders = client.db.reminders
 
-        const expired = reminders.filter((rm) => rm.time < new Date().getTime())
+//         const expired = reminders.filter((rm) => rm.time < new Date().getTime())
 
-        if (expired.length) {
-            for (const reminder of expired) {
-                const user = await client.users.fetch(reminder.userId)
-                if (!user) continue
+//         if (expired.length) {
+//             for (const reminder of expired) {
+//                 const user = await client.users.fetch(reminder.userId)
+//                 if (!user) continue
 
-                user.send({
-                    embeds: [
-                        new MessageEmbed()
-                            .setTitle('Reminder')
-                            .setColor('YELLOW')
-                            .setDescription(
-                                `<t:${(
-                                    (new Date().getTime() - reminder.time) /
-                                    1000
-                                ).toFixed(
-                                    0
-                                )}:R> you asked me to remind you about "${
-                                    reminder.reason
-                                }"`
-                            )
-                            .addField(
-                                'Message Link',
-                                `[Jump](${reminder.link})`
-                            )
-                            .setTimestamp(),
-                    ],
-                })
-                const db = require('../database/models/remind')
-                await db.deleteOne(
-                    {
-                        userId: reminder.userId,
-                        time: reminder.time,
-                    },
-                    {},
-                    {}
-                )
-                client.db.reminders = client.db.reminders.filter(
-                    (a) =>
-                        a.userId !== reminder.userId && a.time !== reminder.time
-                )
-            }
-        }
-    },
-}
+//                 user.send({
+//                     embeds: [
+//                         new MessageEmbed()
+//                             .setTitle('Reminder')
+//                             .setColor('YELLOW')
+//                             .setDescription(
+//                                 `<t:${(
+//                                     (new Date().getTime() - reminder.time) /
+//                                     1000
+//                                 ).toFixed(
+//                                     0
+//                                 )}:R> you asked me to remind you about "${
+//                                     reminder.reason
+//                                 }"`
+//                             )
+//                             .addField(
+//                                 'Message Link',
+//                                 `[Jump](${reminder.link})`
+//                             )
+//                             .setTimestamp(),
+//                     ],
+//                 })
+//                 const db = require('../database/models/remind')
+//                 await db.deleteOne(
+//                     {
+//                         userId: reminder.userId,
+//                         time: reminder.time,
+//                     },
+//                     {},
+//                     {}
+//                 )
+//                 client.db.reminders = client.db.reminders.filter(
+//                     (a) =>
+//                         a.userId !== reminder.userId && a.time !== reminder.time
+//                 )
+//             }
+//         }
+//     },
+// }
