e7f5da6fffa777a8332cb6349c8b637514e24efe
Fellintron <fellintron@gmail.com>
2022-04-02 15:25:45 +0530

    we don't talk about this either

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ client.on('ready', async () => {
     console.log('Logged in.')
     client.emit('tick')
     client.user.setActivity({
-        name: 'meow UwU',
+        name: 'nya UWu 😼',
         type: 'STREAMING',
     })
     client.db.fighthub = client.guilds.cache.get(config.guildId)
