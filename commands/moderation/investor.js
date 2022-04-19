1f3d60292c7fad5679b4707fd32b7d82eecc820b
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-04-19 18:06:43 +0530

    investor command

--- commands/moderation/investor.js
+++ commands/moderation/investor.js
@@ -1,6 +1,87 @@
 const { Message } = require('discord.js')
-
+const db = require('../../database/models/user')
+const { getMilliseconds } = require('better-ms')
 module.exports = {
     name: 'investor',
-    subcommands: ['show', 'add', 'remove'],
+    subcommands: ['show', 'days'],
+    category: 'Donation',
+    /**
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    async execute(message, args) {
+        if (!message.member.permissions.has('ADMINISTRATOR')) {
+            return message.reply('Only admins can run this command.')
+        }
+        const example = `\n\nExamples: \`fh investor show\`, \`fh investor days 598918643727990784 +30\` or \`fh investor days 598918643727990784 -10\``
+        if (!args[0]) {
+            return message.reply("Tell me what i'm supposed to do." + example)
+        }
+
+        if (!this.subcommands.includes(args[0].toLocaleLowerCase())) {
+            return message.reply("Tell me what i'm supposed to do." + example)
+        }
+        const todo = args[0].toLowerCase()
+
+        if (todo === 'show') {
+            const all = await db.find({
+                'fighthub.investor.isInvestor': true,
+            })
+            const mapp = all
+                .sort((a, b) => b.investor.expiresOn - a.investor.expiresOn)
+                .map((v, i) => {
+                    ;`${i + 1}: <@${
+                        v.userId
+                    }> exipres ${message.client.functions.formatTime(
+                        v.investor.expiresOn,
+                        'R'
+                    )}`
+                })
+                .join('\n')
+
+            await message.channel.send({
+                embeds: [
+                    {
+                        title: 'Investors',
+                        color: 'GREEN',
+                        timestamp: new Date(),
+                        description: `**To add/remove days, type \`fh investor days +/-<amount>\`**\n\n${mapp}`,
+                    },
+                ],
+            })
+        } else {
+            args.shift()
+            if (!args[0]) return message.reply('Provide a user id next time.')
+            let user
+            try {
+                user = await message.guild.members.fetch({ user: args[0] })
+            } catch (e) {
+                return message.reply(
+                    `Could not find that user in this guild.\nError: ${e.message}`
+                )
+            }
+            let dbUser = await db.findOne({
+                userId: user.id,
+            })
+            if (!dbUser) {
+                dbUser = new db({
+                    userId: user.id,
+                })
+            }
+            if (!dbUser.investor) {
+                dbUser.investor = {
+                    isInvestor: false,
+                    expiresOn: new Date().getTime(),
+                }
+            }
+            args.shift()
+            if (!args[0])
+                return message.reply('Provide number of days next time.')
+
+            const amount = getMilliseconds(`${args[0]}d`)
+            dbUser.investor.expiresOn += amount
+            dbUser.save()
+            message.react('☑')
+        }
+    },
 }
--- database/models/user.js
+++ database/models/user.js
@@ -15,6 +15,10 @@ const UserSchema = new mongoose.Schema({
             lastVoted: Number,
             enabled: Boolean,
         },
+        investor: {
+            isInvestor: Boolean,
+            expiresOn: Number,
+        },
     },
 })
 
