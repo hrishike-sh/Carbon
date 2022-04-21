713853b5588d3ff8f37a9b88b1a379b87bd50a1f
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-04-21 10:57:49 +0530

    test 1

--- commands/donations/lb.js
+++ commands/donations/lb.js
@@ -2,30 +2,74 @@ const Messages = require('discord-messages')
 const Heists = require('../../functions/heist-dono')
 const Special = require('../../functions/another-dono-thing-whyy')
 const { MessageEmbed: Embed } = require('discord.js')
-
+const { Message, Client, MessageEmbed } = require('discord.js')
 module.exports = {
     name: 'lb',
     aliases: ['lb', 'leaderboard'],
     args: true,
+    fhOnly: true,
     description: 'Check leaderboard for donations',
+    /**
+     * @param {Message} message
+     * @param {String[]} args
+     * @param {Client} client
+     * @returns
+     */
     async execute(message, args, client) {
-        if (!args[0])
-            return message.channel.send(
-                'Which leaderboard would you like to see?\n\nExample: `fh lb h`, `fh lb d`'
+        const mainMessage = await message.channel.send('Loading...')
+        const dLb = (
+            await Messages.computeLeaderboard(
+                client,
+                await Messages.fetchLeaderboard(message.guild.id, 10),
+                true
             )
-        if (message.guild.id !== client.config.guildId) {
-            args[0] = 'd'
-        }
+        ).map(
+            (e) =>
+                `${e.position}. ${e.username}#${
+                    e.discriminator
+                }\nDonated coins: **${e.messages.toLocaleCase()}**`
+        )
+        const hLb = (
+            await Heists.computeLeaderboard(
+                client,
+                await Heists.fetchLeaderboard(message.guild.id, 10),
+                true
+            )
+        ).map(
+            (e) =>
+                `${e.position}. ${e.username}#${
+                    e.discriminator
+                }\nDonated coins: **${e.amount.toLocaleCase()}**`
+        )
+        const fLb = (
+            await Special.computeLeaderboard(
+                client,
+                await Special.fetchLeaderboard(message.guild.id, 10),
+                true
+            )
+        ).map(
+            (e) =>
+                `${e.position}. ${e.username}#${
+                    e.discriminator
+                }\nDonated coins: **${e.amount.toLocaleCase()}**`
+        )
+        const embed = new MessageEmbed().setColor('YELLOW').setTimestamp()
+        embed.setDescription(dLb.join('\n\n'))
+        const collector = await mainMessage.edit({
+            content: '',
+            embeds: [embed],
+        })
+
         if (args[0] === 'd') {
-            const rawLB = await Messages.fetchLeaderboard(message.guild.id, 10)
-            if (rawLB.length < 1)
+            const rawLBD = await Messages.fetchLeaderboard(message.guild.id, 10)
+            if (rawLBD.length < 1)
                 return message.channel.send(
                     'Nobody has done any donations yet... or they are not counted yet.'
                 )
 
             const leaderboard = await Messages.computeLeaderboard(
                 client,
-                rawLB,
+                rawLBD,
                 true
             )
 
@@ -38,14 +82,14 @@ module.exports = {
 
             message.channel.send(`**__LEADERBOARD__**\n\n${lb.join('\n\n')}`)
         } else if (args[0] === 'h') {
-            const rawLB = await Heists.fetchLeaderboard(message.guild.id, 10)
-            if (rawLB.length < 1)
+            const rawLBH = await Heists.fetchLeaderboard(message.guild.id, 10)
+            if (rawLBH.length < 1)
                 return message.channel.send(
                     'Nobody has done any donations yet... or they are not counted yet.'
                 )
             const leaderboard = await Heists.computeLeaderboard(
                 client,
-                rawLB,
+                rawLBH,
                 true
             )
             const lb = leaderboard.map(
@@ -57,14 +101,14 @@ module.exports = {
 
             message.channel.send(`**__LEADERBOARD__**\n\n${lb.join('\n\n')}`)
         } else if (args[0] === 'ff' || args[0] === 'special') {
-            const rawLB = await Special.fetchLeaderboard(message.guild.id, 10)
-            if (rawLB.length < 1)
+            const rawLBF = await Special.fetchLeaderboard(message.guild.id, 10)
+            if (rawLBF.length < 1)
                 return message.channel.send(
                     'Nobody has done any donations yet... or they are not counted yet.'
                 )
             const leaderboard = await Special.computeLeaderboard(
                 client,
-                rawLB,
+                rawLBF,
                 true
             )
             const lb = leaderboard.map(
