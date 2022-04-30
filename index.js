20f1447ee053151ffbfea08c2f818920ab235860
Fellintron <fellintron@gmail.com>
2022-04-30 15:44:55 +0530

    not needed anymore

--- index.js
+++ index.js
@@ -166,17 +166,6 @@ client.on('ready', async () => {
     // Disabled Commands
 }) 
 
- client.on('messageCreate', async (message) => {
-    if (message.channel.id !== '924850662410453042') {
-    return null
-    }
-    
-    await message.react('962407474059694200');
-    await client.functions.sleep(1250);
-    await message.react('962407492300705834');
-    return null;
-    })
-
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return
 
