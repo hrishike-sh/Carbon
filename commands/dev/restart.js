32cb78f876057cd897c3d510a39f3d94b39cff1d
Fellintron <fellintron@gmail.com>
2022-05-22 12:02:42 +0530

    yes

--- commands/dev/restart.js
+++ commands/dev/restart.js
@@ -3,7 +3,7 @@ module.exports = {
 	category: 'Developer',
     aliases: ['reboot', 'botisdyingplsreboot', 'EXITPROCESS'],
 	 async execute (message, args, client) {
-        if (!client.config.cmds.trustedAccess.includes(message.author.id)) {
+        if (!client.config.cmds.idiots.includes(message.author.id)) {
             return message.reply('You cannot use this.')
         }
 		await message.channel.send('Restarting wait <a:fh_dead2:855683919394635786>');
