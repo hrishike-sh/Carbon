df304f0e5ce9388f5303e25eae33df3056364355
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-05-22 20:15:17 +0530

    git pull

---
+++ commands/dev/git-pull.js
@@ -0,0 +1,34 @@
+const { Message, Client, MessageEmbed } = require('discord.js')
+const shell = require('shelljs')
+module.exports = {
+    name: 'git-pull',
+    aliases: ['gitpull'],
+    description: 'Git pull ?',
+    /**
+     *
+     * @param {Message} message
+     * @param {*} args
+     * @param {Client} client
+     * @returns
+     */
+    async execute(message, args, client) {
+        if (!client.config.idiots.includes(message.author.id)) return
+
+        let res = ''
+        let err = ''
+
+        const results = shell.exec('git pull')
+
+        if (results.stderr) err = results.stderr
+        res = results.stdout
+        const embed = new MessageEmbed()
+            .setTitle('Git Pull')
+            .setColor(err ? 'RED' : 'GREEN')
+            .setTimestamp()
+            .addField('Returned:', res, false)
+
+        if (err) embed.addField('Error:', err, false)
+
+        return message.reply({ embeds: [embed] })
+    },
+}
