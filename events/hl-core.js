4056ee71e0ea71c91ace5cb9b8a9930ccf230e14
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-06-07 13:54:07 +0530

    added cd

--- events/hl-core.js
+++ events/hl-core.js
@@ -96,7 +96,7 @@ module.exports = {
 }
 
 const addUser = async (user, client) => {
-    // talked.push(user.id)
-    // await client.functions.sleep(60 * 1000)
-    // talked.splice(talked.indexOf(user.id), 1)
+    talked.push(user.id)
+    await client.functions.sleep(60 * 1000)
+    talked.splice(talked.indexOf(user.id), 1)
 }
