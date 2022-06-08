87eb15bd41b017cbe05450cad62269351b95f35b
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-06-08 14:16:03 +0530

    f

--- commands/Fights/fight.js
+++ commands/Fights/fight.js
@@ -1,4 +1,9 @@
-const { Message } = require('discord.js')
+const {
+    Message,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed,
+} = require('discord.js')
 let fighting = []
 module.exports = {
     name: 'fight',
@@ -14,172 +19,113 @@ module.exports = {
      * @param {String[]} args
      */
     async execute(message, args) {
-        const getRandom = message.client.functions.getRandom
-        const actions = `Type **\`punch\`**, **\`kick\`**, **\`heal\`** or **\`end\`**`
-        const actionArray = ['kick', 'punch', 'heal', 'end']
-        const target = message.mentions.members.first() || null
-
-        if (!target)
-            return message.reply('You must mention someone to fight with.')
-
-        if (
-            fighting.includes(target.id) ||
-            fighting.includes(message.author.id)
-        ) {
-            return message.reply(
-                `Either you or your opponent is already in a fight!`
-            )
+        const CONSTANTS = {
+            dmg: {
+                max: 40,
+                min: 10,
+            },
+            heal: {
+                max: 20,
+                min: 10,
+            },
         }
-        if (target.user.bot) return message.reply('The bot would win.')
+
+        const target = message.mentions.users?.first()
+        if (!target)
+            return message.reply(`You have to mention someone to fight with.`)
+
+        if (target.bot) return message.reply(`The bot won!`)
         if (target.id === message.author.id)
-            return message.reply('You fight yourself... and win!')
-        const gamedata = [
-            {
-                userId: message.author.id,
-                hp: 100,
-                name: message.author.username,
-                mention: `<@${message.author.id}>`,
-            },
-            {
-                userId: target.id,
-                hp: 100,
-                name: target.user.username,
-                mention: `<@${target.id}>`,
-            },
-        ]
-        ;[(target.id, message.author.id)].forEach((a) => {
-            fighting.push(a)
+            return message.reply('The bot does not promote self-harm.')
+
+        const confirMessage = await message.reply({
+            content: `${target.toString()} do you accept their challenge?`,
+            components: [
+                new MessageActionRow().addComponents([
+                    new MessageButton()
+                        .setCustomId('fight-yes')
+                        .setStyle('SUCCESS')
+                        .setLabel('Accept'),
+                    new MessageButton()
+                        .setCustomId('fight-no')
+                        .setStyle('DANGER')
+                        .setLabel('Deny'),
+                ]),
+            ],
         })
-
-        let current = gamedata[getRandom(0, 2)]
-        await message.channel.send(
-            `${current.mention} it's your turn! What do you want to do?\n${actions}`
-        )
-
-        const collector = message.channel.createMessageCollector({
-            filter: (m) =>
-                [gamedata[0].userId, gamedata[1].userId].includes(m.author.id),
+        const collector = confirMessage.createMessageComponentCollector({
+            filter: (b) => {
+                if (b.user.id !== target.id) {
+                    return b.reply({
+                        content: `Only ${target.toString()} can use this interaction!`,
+                        ephemeral: true,
+                    })
+                } else return true
+            },
+            idle: 15 * 1000,
         })
+        let status = 'no response'
 
-        collector.on('collect', async (msg) => {
-            if (msg.author.id !== current.userId) return
-
-            if (!actionArray.includes(msg.content.toLowerCase()))
-                return msg.reply(
-                    `${current.mention} that is not a valid option bozo.\n${actions}`
-                )
-
-            const action = msg.content.toLowerCase()
-            const opponent = gamedata.filter(
-                (val) => val.userId !== current.userId
-            )[0]
-
-            if (action === 'punch') {
-                const damage = getRandom(5, 25)
-                opponent.hp -= damage
-
-                const m = await msg.reply(
-                    `**Punch** | **${current.name}** punches ${
-                        opponent.name
-                    } and deals **${damage}** damage!\n${
-                        opponent.name
-                    } is left with ${
-                        opponent.hp < 1 ? '0' : opponent.hp
-                    } health!`
-                )
-                if (checkDeath(opponent)) {
-                    msg.channel.send(
-                        `🏆🏆 **${current.name} has won the game** 🏆🏆`
-                    )
-                    collector.stop()
-                } else {
-                    m.edit(
-                        `${m.content}\n\n${opponent.mention} its your turn! What do you want to do?\n${actions}`
-                    )
-                    current = opponent
-                }
-            } else if (action === 'kick') {
-                const fall = [1, 1, 1, 2][getRandom(0, 4)] === 2
-
-                if (fall) {
-                    const fallDamage = getRandom(8, 20)
-                    current.hp -= fallDamage
-
-                    const m = await msg.reply(
-                        `**Fall** | **${current.name}** tried to kick ${
-                            opponent.name
-                        } but FELL!! They took **${fallDamage}** damage.\nYou are now at **${
-                            current.hp < 1 ? '0' : current.hp
-                        }** health!`
-                    )
-                    if (checkDeath(current)) {
-                        msg.channel.send(
-                            `🏆🏆 **${opponent.name} has won the game** 🏆🏆`
-                        )
-                        collector.stop()
-                    } else {
-                        m.edit(
-                            `${m.content}\n\n${opponent.mention} its your turn! What do you want to do?\n${actions}`
-                        )
-                        current = opponent
-                    }
-                } else {
-                    const damage = getRandom(20, 40)
-                    opponent.hp -= damage
-
-                    const m = await msg.reply(
-                        `**Kick** | **${current.name}** kicks ${
-                            opponent.name
-                        } and deals **${damage}** damage!\n${
-                            opponent.name
-                        } is at ${opponent.hp < 1 ? '0' : opponent.hp} health!`
-                    )
-
-                    if (checkDeath(opponent)) {
-                        msg.channel.send(
-                            `🏆🏆 **${current.name} has won the game** 🏆🏆`
-                        )
-                        collector.stop()
-                    } else {
-                        m.edit(
-                            `${m.content}\n\n${opponent.mention} its your turn! What do you want to do?\n${actions}`
-                        )
-                        current = opponent
-                    }
-                }
-            } else if (action === 'heal') {
-                const heal = getRandom(5, 15)
-
-                if (current.hp > 99) {
-                    msg.reply(
-                        `You tried to heal but you're already at 100HP! You lost your turn!\n${opponent.mention} its your turn! What do you want to do?\n${actions}`
-                    )
-                    current = opponent
-                } else {
-                    current.hp += heal
-
-                    msg.reply(
-                        `**Heal** | **${current.name}** has healed **${heal}** health! They are now at ${current.hp} health.\n${opponent.mention} its your turn! What do you want to do?\n${actions}`
-                    )
-
-                    current = opponent
-                }
-            } else if (action === 'end') {
+        collector.on('collect', async (btn) => {
+            if (btn.customId.includes('no')) {
                 collector.stop()
+                status = 'rejected lol'
+                btn.deferUpdate()
+                return
+            }
+            status = 'asdkjgnasg'
 
-                message.channel.send(
-                    `🏆🏆 **${opponent.name} has won the game** 🏆🏆`
+            const gamedata = [
+                {
+                    user: message.author,
+                    hp: 100,
+                    dead: false,
+                },
+                {
+                    user: target,
+                    hp: 100,
+                    dead: false,
+                },
+            ]
+            let current = gamedata[Math.floor(Math.random() * 2)]
+
+            const fightEmbed = new MessageEmbed()
+                .setTitle('Fight')
+                .addField(gamedata[0].user.tag, `Health: **100 %**`, true)
+                .addField(gamedata[1].user.tag, `Health: **100 %**`, true)
+                .addField('Last Action', 'The game has started!', false)
+
+            const mainMessage = await message.channel.send({
+                embeds: [fightEmbed],
+            })
+        })
+
+        collector.on('end', () => {
+            if (status.includes('respon')) {
+                confirMessage.components[0].components.forEach((c) =>
+                    c.setDisabled()
                 )
+                confirMessage.edit({
+                    content: `~~${confirMessage.content}~~\n\nNo response!`,
+                    components: confirMessage.components,
+                })
+            } else if (status.includes('rejec')) {
+                confirMessage.components[0].components.forEach((c) =>
+                    c.setDisabled()
+                )
+                confirMessage.edit({
+                    content: `~~${confirMessage.content}~~\n\nThe challenge was denied.`,
+                    components: confirMessage.components,
+                })
+            } else {
+                confirMessage.components[0].components.forEach((c) =>
+                    c.setDisabled()
+                )
+                confirMessage.edit({
+                    content: `The challenge was accepted.`,
+                    components: confirMessage.components,
+                })
             }
         })
-        collector.on('end', () => {
-            fighting = fighting.filter(
-                (a) => ![target.id, message.author.id].includes(a)
-            )
-        })
     },
-}
-
-const checkDeath = (cur) => {
-    return cur.hp < 1
 }
