249d13de543d4ee4475ca1d00e16debe79393823
Fellintron <fellintron@gmail.com>
2022-07-15 16:48:50 +0530

    yes

--- database/models/user.js
+++ database/models/user.js
@@ -1,28 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    userId: String,
-    commandsRan: { type: Number, default: 0 },
-    afk: {
-        type: Object,
-        default: {
-            afk: false,
-        },
+  userId: String,
+  commandsRan: { type: Number, default: 0 },
+  afk: {
+    type: Object,
+    default: {
+      afk: false
+    }
+  },
+  highlight: {
+    words: [String]
+  },
+  messageSettings: {
+    highlightsDisabled: Boolean,
+    snipesDisabled: Boolean,
+    lastPingDisabled: Boolean
+  },
+  fighthub: {
+    voting: {
+      hasVoted: Boolean,
+      lastVoted: Number,
+      enabled: Boolean
     },
-    highlight: {
-        words: [String],
-    },
-    fighthub: {
-        voting: {
-            hasVoted: Boolean,
-            lastVoted: Number,
-            enabled: Boolean,
-        },
-        investor: {
-            isInvestor: Boolean,
-            expiresOn: Number,
-        },
-    },
-})
+    investor: {
+      isInvestor: Boolean,
+      expiresOn: Number
+    }
+  }
+});
 
-module.exports = mongoose.model('user', UserSchema)
+module.exports = mongoose.model('user', UserSchema);
