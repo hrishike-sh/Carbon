86743a8bf0debf2c5bcf7491cff91f16b418e001
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-07-29 20:16:55 +0530

    yes

--- slashcommands/ping.js
+++ slashcommands/ping.js
@@ -1,28 +1,45 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Check my round trip and Discord websocket latency.'),
     category: 'Utility',
+    global: true,
     async execute(interaction) {
-        const { client } = interaction;
+        const { client } = interaction
 
         try {
-            await interaction.reply({ content: 'Pinging...' });
-            
+            await interaction.reply({ content: 'Pinging...' })
+
             const embed = new MessageEmbed()
                 .addFields(
-                    { name: 'Discord WebSocket', value: `${client.ws.ping.toLocalString()}ms` },
-                    { name: 'Round Trip', value: `${(Date.now() - interaction.createdTimestamp).toLocaleString()}ms` },
+                    {
+                        name: 'Discord WebSocket',
+                        value: `${client.ws.ping.toLocalString()}ms`,
+                    },
+                    {
+                        name: 'Round Trip',
+                        value: `${(
+                            Date.now() - interaction.createdTimestamp
+                        ).toLocaleString()}ms`,
+                    }
                 )
-                .setFooter({ text: client.user.tag, iconURL: client.user.displayAvatarURL({ dynamic: true }) ?? interaction.user.displayAvatarURL({ dynamic: true }) })
-                .setTimestamp();
-            
-            await interaction.editReply({ content: '🏓 Pong!', embeds: [embed] });
+                .setFooter({
+                    text: client.user.tag,
+                    iconURL:
+                        client.user.displayAvatarURL({ dynamic: true }) ??
+                        interaction.user.displayAvatarURL({ dynamic: true }),
+                })
+                .setTimestamp()
+
+            await interaction.editReply({
+                content: '🏓 Pong!',
+                embeds: [embed],
+            })
         } catch (error) {
-            console.error(error);
+            console.error(error)
         }
     },
-};
+}
