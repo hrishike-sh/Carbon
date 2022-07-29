04cac405e91d99a805355498a03e2335b2d5038c
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-07-29 20:02:38 +0530

    globalcommands

--- deploy.js
+++ deploy.js
@@ -4,25 +4,45 @@ const { Routes } = require('discord-api-types/v9')
 const { clientId, guildId } = require('./config.json')
 
 const commands = []
+const globalCommands = []
 const commandFiles = fs
     .readdirSync('./slashcommands')
     .filter((file) => file.endsWith('.js'))
 for (const file of commandFiles) {
     const command = require(`./slashcommands/${file}`)
-    commands.push(command.data.toJSON())
+    if (command.global) {
+        globalCommands.push(command.data.toJSON())
+    } else {
+        commands.push(command.data.toJSON())
+    }
 }
 
 const rest = new REST({ version: '9' }).setToken(process.env.token)
 
 ;(async () => {
     try {
-        console.log('Started refreshing application (/) commands.')
+        console.log('[FightHub] Started refreshing application (/) commands.')
 
         await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
             body: commands,
         })
 
-        console.log('Successfully reloaded application (/) commands.')
+        console.log(
+            '[FightHub] Successfully reloaded application (/) commands.'
+        )
+    } catch (error) {
+        console.error(error)
+    }
+})()
+;(async () => {
+    try {
+        console.log('[GLOBAL]: Started refreshing application (/) commands.')
+
+        await rest.put(Routes.applicationGuildCommands(clientId), {
+            body: globalCommands,
+        })
+
+        console.log('[GLOBAL]: Successfully reloaded application (/) commands.')
     } catch (error) {
         console.error(error)
     }
--- slashcommands/gconfig.js
+++ slashcommands/gconfig.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 const { CommandInteraction, Client, MessageEmbed } = require('discord.js')
 const Database = require('../database/models/settingsSchema')
 module.exports = {
+    global: true,
     data: new SlashCommandBuilder()
         .setName('gconfig')
         .setDescription("Configure your server's giveaway settings!")
