7c571aa872a1cd62da827547f90576dcb63d3725
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-07-29 15:45:49 +0530

    ok

--- commands/util/whois.js
+++ commands/util/whois.js
@@ -1,4 +1,4 @@
-const { Formatters, MessageEmbed } = require('discord.js');
+const { Formatters, MessageEmbed } = require('discord.js')
 
 module.exports = {
     name: 'whois',
@@ -7,29 +7,44 @@ module.exports = {
     usage: '[user]',
     async execute(message, args, client) {
         try {
-            const user = message.mentions.users.last() || (await client.users.fetch(args[0]).catch(() => null)) || client.users.cache.find((user) => user.username === args[0] || user.tag === args[0]) || message.author;
-        
-            const mutuals = new Array();
-        
-            const guilds = client.guilds.cache.values();
-            
+            const user =
+                message.mentions.users.last() ||
+                (await client.users.fetch(args[0]).catch(() => null)) ||
+                client.users.cache.find(
+                    (user) => user.username === args[0] || user.tag === args[0]
+                ) ||
+                message.author
+
+            const mutuals = new Array()
+
+            const guilds = client.guilds.cache.values()
+
             for (const guild of guilds) {
-                if (guild.members.cache.has(user.id) {
-                    mutuals.push(`${Formatters.inlineCode(guild.id)} - ${Formatters.bold(guild.name)}`)
+                if (guild.members.cache.has(user.id)) {
+                    mutuals.push(
+                        `${Formatters.inlineCode(guild.id)} - ${Formatters.bold(
+                            guild.name
+                        )}`
+                    )
                 } else {
-                    continue;
+                    continue
                 }
             }
-        
-            const embed = new MessageEmbed()
-                .setAuthor({ name: `${user.tag} - ${user.id}`, iconURL: user.displayAvatarURL({ dynamic: true }) })
-                .setThumbnail(user.displayAvatarURL({ dynamic: true }))
-                .setDescription(`${Formatters.bold('Mutual Guilds')}\n${mutuals.join('\n')}`)
-                .setColor('RANDOM');
 
-            await message.channel.send({ embeds: [embed] });
+            const embed = new MessageEmbed()
+                .setAuthor({
+                    name: `${user.tag} - ${user.id}`,
+                    iconURL: user.displayAvatarURL({ dynamic: true }),
+                })
+                .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+                .setDescription(
+                    `${Formatters.bold('Mutual Guilds')}\n${mutuals.join('\n')}`
+                )
+                .setColor('RANDOM')
+
+            await message.channel.send({ embeds: [embed] })
         } catch (error) {
-            console.error(error);
+            console.error(error)
         }
     },
-};
+}
