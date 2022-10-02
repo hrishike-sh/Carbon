2b1bbdf528f69f3e60ae091a1cbb750b2b8986e8
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-10-02 23:15:10 +0530

    oKay

--- events/timer.js
+++ events/timer.js
@@ -3,11 +3,13 @@ const {
     Message,
     MessageActionRow,
     MessageButton,
+    Collection,
 } = require('discord.js')
 const { splitMessage } = require('../node_modules/discord.js/src/util/Util')
 const timers = require('../database/models/timer')
 const ms = require('better-ms')
 let counter = 0
+const toEdit = new Collection()
 module.exports = {
     name: 'tick',
     /**
@@ -36,6 +38,45 @@ module.exports = {
                 const message = await channel.messages.fetch(timer.messageId)
                 if (!message) return
                 clearTimer(message, timer.time, timer.reminders)
+                toEdit.set(timer.messageId, 'a')
+                setTimeout(async () => {
+                    toEdit.delete(timer.messageId)
+                    const a = await timers.findOne({
+                        messageId: timer.messageId,
+                    })
+                    const em = message.embeds[0]
+                        .setDescription(
+                            `Ended ${client.functions.format(timer.time)}`
+                        )
+                        .setFooter(
+                            `Reminded a total of ${a.reminders.length.toLocaleString()} users!`
+                        )
+                    message.edit({ embeds: em })
+
+                    const z = a.reminders.length
+                        ? a.reminders.map((a) => `<@${a}>`).join('')
+                        : client.user.id.toString()
+                    const messages = splitMessage(z)
+
+                    for await (const msg of messages) {
+                        await message.channel.send(msg).then(async (a) => {
+                            await message.client.functions.sleep(1000)
+                            a.delete()
+                        })
+                    }
+
+                    message.channel.send({
+                        content: `The timer for **${message.embeds[0].title}** has ended!`,
+                        components: [
+                            new MessageActionRow().addComponents([
+                                new MessageButton()
+                                    .setLabel('Jump')
+                                    .setStyle('LINK')
+                                    .setURL(message.url),
+                            ]),
+                        ],
+                    })
+                }, timer.time - n)
             } else continue
         }
     },
@@ -46,32 +87,6 @@ module.exports = {
  * @param {Number} time
  */
 const clearTimer = async (message, time, arr) => {
-    if (time < new Date().getTime()) {
-        const z = arr.length
-            ? arr.map((a) => `<@${a}>`).join('')
-            : message.client.user.id.toString()
-        const messages = splitMessage(z)
-
-        for await (const msg of messages) {
-            await message.channel.send(msg).then(async (a) => {
-                await message.client.functions.sleep(1000)
-                a.delete()
-            })
-        }
-
-        message.channel.send({
-            content: `The timer for **${message.embeds[0].title}** has ended!`,
-            components: [
-                new MessageActionRow().addComponents([
-                    new MessageButton()
-                        .setLabel('Jump')
-                        .setStyle('LINK')
-                        .setURL(message.url),
-                ]),
-            ],
-        })
-        return
-    }
     const formatted = ms.prettyMs(
         Number((time - new Date().getTime()).toString().slice(0, -3) + '000'),
         {
@@ -85,5 +100,5 @@ const clearTimer = async (message, time, arr) => {
 
     await setTimeout((a) => {
         clearTimer(message, time, arr)
-    }, 10_000)
+    }, 5000)
 }
