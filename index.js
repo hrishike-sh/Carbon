6fa0925f2a17fb39cd2802868727ca45d47fb903
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-10-20 22:44:04 +0530

    back

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const client = new Client({
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_BANS,
     ],
+    partials: [Partials.Reaction],
 })
 
 let dbURL = process.env.mongopath
