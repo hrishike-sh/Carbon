d0fd684ad8cf220f3e70676b7d6cc473f0db688c
Dauntless7 <nicolai.8293@gmail.com>
2022-11-21 17:50:11 -0400

    fix? idk

--- events/skullAdd.js
+++ events/skullAdd.js
@@ -1,94 +1,83 @@
-const {
-    ActionRowBuilder,
-    ButtonBuilder,
-    EmbedBuilder,
-    ButtonStyle,
-    TextChannel,
-} = require('discord.js')
-const noSpam = []
-const serverSettings = require('../database/models/settingsSchema')
-const skulls = require('../database/models/skullboard')
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
+const serverSettings = require('../database/models/settingsSchema');
+const skulls = require('../database/models/skullboard');
+const noSpam = [];
 module.exports = {
-    name: 'messageReactionAdd',
-    once: false,
-    async execute(reaction, user, client) {
-        if (reaction.emoji.name !== '💀') return
+  name: 'messageReactionAdd',
+  once: false,
+  async execute(reaction, user, client) {
+    if (!reaction.emoji.name || reaction.emoji.name !== '💀') return;
 
-        const message = reaction.message
-        const valid = await serverSettings.findOne({
-            guildID: message.guild.id,
-        })
-        if (!valid) return
-        if (!valid?.skullBoard.enabled) return
-        const { count, channelId } = valid.skullBoard
-        const rec = await reaction.fetch()
-        if (rec.count < count) return
-        let exists = await skulls.findOne({
-            messageId: message.id,
-        })
-        if (exists) {
-            console.log('it exists')
-            exists.count++
-            const c = client.channels.cache?.get(channelId)
-            let msg = await c.messages.fetch(exists.skullBoardMessageId)
-            console.log(msg)
-            const ee = EmbedBuilder.from(msg.embeds[0])
-            ee.setTitle(`**${reaction.count} :skull:**`)
-            //msg.embeds[0].setTitle(`**${exists.count} :skull:**`)
-            msg.edit({
-                embeds: [ee],
-            })
-            exists.save()
-            return
-        } else {
-            if (noSpam.includes(message.id)) {
-                return
-            } else {
-                noSpam.push(message.id)
-            }
-            exists = {
-                messageId: message.id,
-                count: reaction.count,
-            }
-        }
+    const message = reaction.message;
+    const valid = await serverSettings.findOne({
+      guildID: message.guild.id
+    });
+    if (!valid) return;
+    if (!valid?.skullBoard.enabled) return;
+    const { count, channelId } = valid.skullBoard;
+    const rec = await reaction.fetch();
+    if (rec.count < count) return;
+    let exists = client.skulls.get(message.id);
+    if (exists) {
+      console.log('it exists');
+      // exists.count++;
+      const c = client.channels.cache?.get(channelId);
+      let msg = await c.messages.fetch(exists);
+      console.log(msg);
+      const ee = EmbedBuilder.from(msg.embeds[0]);
+      ee.setTitle(`**${reaction.count} :skull:**`);
+      //msg.embeds[0].setTitle(`**${exists.count} :skull:**`)
+      msg.edit({
+        embeds: [ee]
+      });
+      // exists.save();
+      return;
+    } else {
+      if (noSpam.includes(message.id)) {
+        return;
+      } else {
+        noSpam.push(message.id);
+      }
+      client.skulls.set(message.id, null);
+    }
 
-        const embed = new EmbedBuilder()
-            .setAuthor({
-                name: message.author.tag,
-                iconURL: message.author.displayAvatarURL(),
-                url: message.url,
-            })
-            .setDescription(message.content || ' ')
-            .setImage(message.attachments.first()?.url)
-            .setFooter({
-                text: 'Use this in your own server by using `/skullboard`!',
-            })
-            .setTitle(`**${reaction.count} :skull:**`)
-        const channel = client.channels.cache.get(channelId)
-        if (!channel) return
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: message.author.tag,
+        iconURL: message.author.displayAvatarURL(),
+        url: message.url
+      })
+      .setDescription(message.content || ' ')
+      .setImage(message.attachments.first()?.url)
+      .setFooter({
+        text: 'Use this in your own server by using `/skullboard`!'
+      })
+      .setTitle(`**${reaction.count} :skull:**`);
+    const channel = client.channels.cache.get(channelId);
+    if (!channel) return;
 
-        const temp = await channel.send({
-            embeds: [embed],
-            components: [
-                new ActionRowBuilder().addComponents([
-                    new ButtonBuilder()
-                        .setEmoji('💀')
-                        .setStyle(5)
-                        .setURL(message.url)
-                        .setLabel('Message'),
-                ]),
-            ],
-        })
+    const temp = await channel.send({
+      embeds: [embed],
+      components: [
+        new ActionRowBuilder().addComponents([
+          new ButtonBuilder()
+            .setEmoji('💀')
+            .setStyle(5)
+            .setURL(message.url)
+            .setLabel('Message')
+        ])
+      ]
+    });
 
-        const z = new skulls({
-            messageId: message.id,
-            channelId: message.channel.id,
-            authorId: message.author.id,
-            guildId: message.guild.id,
-            count: reaction.count,
-            skullBoardMessageId: temp.id,
-        })
-        z.save()
-        noSpam = noSpam.filter((a) => a !== message.id)
-    },
-}
+    const z = new skulls({
+      messageId: message.id,
+      channelId: message.channel.id,
+      authorId: message.author.id,
+      guildId: message.guild.id,
+      count: reaction.count,
+      skullBoardMessageId: temp.id
+    });
+    z.save();
+    noSpam = noSpam.filter((a) => a !== message.id);
+  }
+};
--- events/skullRemove.js
+++ events/skullRemove.js
@@ -1,56 +1,46 @@
-const {
-    ActionRowBuilder,
-    ButtonBuilder,
-    EmbedBuilder,
-    ButtonStyle,
-} = require('discord.js')
-const serverSettings = require('../database/models/settingsSchema')
-const skulls = require('../database/models/skullboard')
+const { EmbedBuilder } = require('discord.js');
+const serverSettings = require('../database/models/settingsSchema');
+// const skulls = require('../database/models/skullboard')
 module.exports = {
-    name: 'messageReactionRemove',
-    once: false,
-    async execute(reaction, user, client) {
-        console.log(`Reaction Removed; name: ${reaction.emoji.name}`)
-        if (reaction.emoji.name !== '💀') return
+  name: 'messageReactionRemove',
+  once: false,
+  async execute(reaction, ueer, client) {
+    console.log(`Reaction Removed: ${reaction.emoji.name}`);
+    if (!reaction.emoji.name || reaction.emoji.name !== '💀') return;
 
-        const message = reaction.message
-        const valid = await serverSettings.findOne({
-            guildID: message.guild.id,
-        })
-        if (!valid) return
-        if (!valid?.skullBoard.enabled) return
+    const message = reaction.message;
+    const valid = await serverSettings.findOne({
+      guildID: message.guild.id
+    });
+    if (!valid) return;
+    if (!valid?.skullBoard.enabled) return;
 
-        const { count, channelId } = valid.skullBoard
-        const rec = await reaction.fetch()
-        const exists = await skulls.findOne({
-            messageId: message.id,
-        })
-        if (rec.count < count) {
-            if (!exists) return
-            const mesId = exists.skullBoardMessageId
-            if (exists) {
-                await skulls.deleteMany({
-                    messageId: message.id,
-                })
-                const c = client.channels.cache.get(valid.skullBoard.channelId)
-                const m = await c.messages.fetch(mesId)
-                if (!m) return
-
-                m.delete()
-                return
-            }
-        } else {
-            exists.count--
-            const c = client.channels.cache?.get(channelId)
-            let msg = await c.messages.fetch(exists.skullBoardMessageId)
-            console.log(msg)
-            const ee = EmbedBuilder.from(msg.embeds[0])
-            ee.setTitle(`**${reaction.count} :skull:**`)
-            //msg.embeds[0].setTitle(`**${exists.count} :skull:**`)
-            msg.edit({
-                embeds: [ee],
-            })
-            exists.save()
-        }
-    },
-}
+    const { count, channelId } = valid.skullBoard;
+    const rec = await reaction.fetch();
+    const exists = client.skulls.get(message.id);
+    if (rec.count < count) {
+      if (!exists) return;
+      const mesId = exists;
+      if (exists) {
+        client.skulls.delete(message.id);
+        const c = client.channels.cache.get(valid.skullBoard.channelId);
+        const m = await c.messages.fetch(mesId);
+        if (!m) return;
+        m.delete();
+        return;
+      }
+    } else {
+      // exists.count--;
+      const c = client.channels.cache?.get(channelId);
+      let msg = await c.messages.fetch(exists);
+      // console.log(msg);
+      const ee = EmbedBuilder.from(msg.embeds[0]);
+      ee.setTitle(`**${reaction.count} :skull:**`);
+      // msg.embeds[0].setTitle(`**${exists.count} :skull:**`)
+      msg.edit({
+        embeds: [ee]
+      });
+      // exists.save();
+    }
+  }
+};
