9f93c9617e5303fc57e66f304e28a9d50abcb51a
Dauntless7 <nicolai.8293@gmail.com>
2022-11-22 10:34:03 -0400

    temp fix #2

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,29 +1,29 @@
-const { Message, Client } = require('discord.js')
-const userSchema = require('../database/models/user')
+const { Message, Client } = require('discord.js');
+// const userSchema = require('../database/models/user')
 
 module.exports = {
-    name: 'messageDelete',
-    once: false,
-    /**
-     *
-     * @param {Message} message
-     * @param {Client} client
-     */
-    async execute(message, client) {
-        const userSettings = await userSchema.findOne({
-            userId: message.author.id,
-        })
+  name: 'messageDelete',
+  once: false,
+  /**
+   *
+   * @param {Message} message
+   * @param {Client} client
+   */
+  async execute(message, client) {
+    // const userSettings = await userSchema.findOne({
+    //     userId: message.author.id,
+    // })
 
-        if (userSettings?.messagesettings?.snipesDisabled === true) return
+    // if (userSettings?.messagesettings?.snipesDisabled === true) return
 
-        let snipes = client.snipes.snipes.get(message.channel.id) || []
+    let snipes = client.snipes.snipes.get(message.channel.id) || [];
 
-        snipes.unshift({
-            msg: message,
-            image: message.attachments.first()?.proxyURL || null,
-            time: Date.now(),
-        })
+    snipes.unshift({
+      msg: message,
+      image: message.attachments.first()?.proxyURL || null,
+      time: Date.now()
+    });
 
-        client.snipes.snipes.set(message.channel.id, snipes)
-    },
-}
+    client.snipes.snipes.set(message.channel.id, snipes);
+  }
+};
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,26 +1,26 @@
-const userSchema = require('../database/models/user')
+// const userSchema = require('../database/models/user')
 
 module.exports = {
-    name: 'messageUpdate',
-    once: false,
-    async execute(oldMessage, newMessage, client) {
-        const userSettings = await userSchema.findOne({
-            userId: newMessage.author.id,
-        })
+  name: 'messageUpdate',
+  once: false,
+  async execute(oldMessage, newMessage, client) {
+    // const userSettings = await userSchema.findOne({
+    //     userId: newMessage.author.id,
+    // })
 
-        if (userSettings?.messagesettings?.snipesDisabled === true) return
+    // if (userSettings?.messagesettings?.snipesDisabled === true) return
 
-        let snipes = client.snipes.esnipes.get(oldMessage.channel.id) || []
+    let snipes = client.snipes.esnipes.get(oldMessage.channel.id) || [];
 
-        snipes.unshift({
-            oldContent: oldMessage.content,
-            newContent: newMessage.content,
-            editedIn: newMessage.createdAt - oldMessage.editedAt,
-            member: newMessage.member,
-            author: newMessage.author,
-            msg: newMessage,
-        })
+    snipes.unshift({
+      oldContent: oldMessage.content,
+      newContent: newMessage.content,
+      editedIn: newMessage.createdAt - oldMessage.editedAt,
+      member: newMessage.member,
+      author: newMessage.author,
+      msg: newMessage
+    });
 
-        client.snipes.esnipes.set(oldMessage.channel.id, snipes)
-    },
-}
+    client.snipes.esnipes.set(oldMessage.channel.id, snipes);
+  }
+};
