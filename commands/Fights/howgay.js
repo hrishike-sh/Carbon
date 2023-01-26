55288a0f7366161048a596fbf92f08111148686b
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2023-01-26 23:25:40 +0530

    howgay fixed - mod alt intel 👀👀

--- commands/Fights/howgay.js
+++ commands/Fights/howgay.js
@@ -1,155 +1,163 @@
-// const {
-//     EmbedBuilder,
-//     ButtonBuilder,
-//     ActionRowBuilder,
-//     Message,
-//     ButtonStyle,
-// } = require('discord.js')
+const {
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  Message,
+  ButtonStyle,
+} = require("discord.js");
 
-// module.exports = {
-//     name: 'howgay',
-//     category: 'Fights',
-//     args: true,
-//     usage: '<user> <high / low>',
-//     description: "Dank Memer's howgay fighthub method, but its automatic!",
-//     /**
-//      *
-//      * @param {Message} message
-//      * @param {String[]} args
-//      * @param {Client} client
-//      */
-//     async execute(message, args, client) {
-//         const target = message.mentions.users?.first()
-//         if (!target)
-//             return message.reply(`You have to mention someone to fight them.`)
-//         args.shift()
-//         const what = args[0]
-//         if (!what || !['low', 'high'].includes(what.toLowerCase()))
-//             return message.reply(
-//                 `You have to specify either high or low.\n\nExample: \`fh howgay @Hrishikesh#0369 low\``
-//             )
-//         const confirmation = (
-//             await message.channel.send({
-//                 content: target.toString(),
-//                 embeds: [
-//                     {
-//                         title: 'Confirmation',
-//                         description: `${message.author.toString()} has challenged you for a game of howgay!\nDo you accept their challenge?`,
-//                         footer: {
-//                             text: 'Use the buttons.',
-//                         },
-//                     },
-//                 ],
-//                 components: [
-//                     new ActionRowBuilder().addComponents([
-//                         new ButtonBuilder()
-//                             .setStyle(ButtonStyle.Success)
-//                             .setCustomId('hg-yes')
-//                             .setLabel('Accept'),
-//                         new ButtonBuilder()
-//                             .setStyle(ButtonStyle.Danger)
-//                             .setCustomId('hg-no')
-//                             .setLabel('Deny'),
-//                     ]),
-//                 ],
-//             })
-//         ).createMessageComponentCollector({
-//             filter: (b) => {
-//                 if (b.user.id !== target.id) {
-//                     return b.reply({
-//                         content: 'This is not your challenge.',
-//                         ephemeral: true,
-//                     })
-//                 } else return true
-//             },
-//             idle: 10 * 1000,
-//         })
+module.exports = {
+  name: "howgay",
+  category: "Fights",
+  args: true,
+  usage: "<user> <high / low>",
+  description: "Dank Memer's howgay fighthub method, but its automatic!",
+  /**
+   *
+   * @param {Message} message
+   * @param {String[]} args
+   * @param {Client} client
+   */
+  async execute(message, args, client) {
+    const target = message.mentions?.users?.first() || null;
 
-//         confirmation.on('collect', async (button) => {
-//             confirmation.stop()
-//             button.deferUpdate()
-//             if (button.customId.includes('no'))
-//                 return message.channel.send(
-//                     `The challange was denied by ${button.user.toString()}`
-//                 )
-//             const gamedata = [
-//                 {
-//                     user: message.author,
-//                     rate: Math.floor(Math.random() * 100),
-//                 },
-//                 {
-//                     user: target,
-//                     rate: Math.floor(Math.random() * 100),
-//                 },
-//             ]
-//             const embed = new EmbedBuilder()
-//                 .setTitle(`Howgay [${what.toUpperCase()}]`)
-//                 .setDescription('The bot will automatically show the winner!')
-//                 .setFooter({
-//                     text: "isn't that gay",
-//                 })
-//                 .setColor('Aqua')
-//             const fancy = await message.channel.send({
-//                 embeds: [embed],
-//             })
-//             await client.functions.sleep(500)
-//             embed.addFields([
-//                 {
-//                     name: `${gamedata[0].user.tag}`,
-//                     value: `Rate: ${gamedata[0].rate}%`,
-//                     inline: true,
-//                 },
-//             ])
-//             await fancy.edit({
-//                 embeds: [embed],
-//             })
-//             await client.functions.sleep(2500)
-//             embed.addFields([
-//                 {
-//                     name: `${gamedata[1].user.tag}`,
-//                     value: `Rate: ${gamedata[1].rate}%`,
-//                     inline: true,
-//                 },
-//             ])
-//             await fancy.edit({
-//                 embeds: [embed],
-//             })
+    if (!target)
+      return message.channel.send("You must ping someone to play with them!");
 
-//             let winner
-//             if (gamedata[0].rate == gamedata[1].rate) {
-//                 winner = "It's a tie!!"
-//             } else {
-//                 if (what === 'low') {
-//                     winner = `${gamedata
-//                         .sort((a, b) => a.rate - b.rate)[0]
-//                         .user.toString()} has won the howgay!`
-//                 } else {
-//                     winner = `${gamedata
-//                         .sort((a, b) => b.rate - a.rate)[0]
-//                         .user.toString()} has won the howgay!`
-//                 }
-//             }
+    let gamedata = {
+      players: {
+        one: message.member,
+        two: message.mentions.members.first(),
+        oneR: getRate(),
+        twoR: getRate(),
+      },
+    };
+    args.shift();
 
-//             fancy.reply(winner)
-//         })
-//         confirmation.on('end', () => {
-//             const msg = message.channel.messages.cache.get(
-//                 confirmation.messageId
-//             )
-//             msg.components[0].components.forEach((c) =>
-//                 ButtonBuilder.from(c).setDisabled()
-//             )
-//             msg.edit({
-//                 components: msg.components,
-//             })
-//         })
-//     },
-// }
+    const type = args[0]?.toLocaleLowerCase() || null;
 
-// function sleep(ms) {
-//     return new Promise((resolve) => setTimeout(resolve, ms))
-// }
+    if (!type || !["low", "high", "l", "h"].includes(type)) {
+      return message.reply(
+        `You must specify the type of fight! Either HIGH or LOW (high/low/h/l).`
+      );
+    }
 
-// /**
-//  */
-// //stfu
+    gamedata.type = type.includes("h") ? "high" : "low";
+
+    const confirmationMessage = await message.channel.send({
+      content: `${target.toString()} do you want to play a game of HowGay with ${message.author.toString()}?`,
+      embeds: [
+        {
+          title: `Confirmation | ${target.username}`,
+          description:
+            "Use the button to make your choice.\nYou have 30 seconds...",
+          color: `Yellow`,
+        },
+      ],
+      components: [
+        new MessageActionRow().addComponents([
+          new MessageButton()
+            .setLabel("Accept")
+            .setSyle(ButtonStyle.Success)
+            .setCustomId("accept-hg"),
+          new MessageButton()
+            .setLabel("Deny")
+            .setSyle(ButtonStyle.Danger)
+            .setCustomId("deny-hg"),
+        ]),
+      ],
+    });
+
+    const collector = confirmationMessage.createMessageComponentCollector({
+      time: 30 * 1000,
+    });
+
+    collector.on("collect", async (button) => {
+      if (button.user.id !== target.user.id) {
+        return button.reply({
+          content: ":warning: This is not for you idiot.",
+          ephemeral: true,
+        });
+      }
+
+      if (button.customId.includes("accept")) {
+        confirmationMessage.edit({
+          embeds: [null],
+          content: "This request was ACCEPTED.",
+          components: [null],
+        });
+
+        const mainMessage = await message.channel.send({
+          embeds: [
+            {
+              title: "Starting game....",
+              color: "Green",
+            },
+          ],
+        });
+
+        await sleep(5000)
+
+        const embed = new EmbedBuilder()
+          .setTitle(`Howgay | ${target.user.tag} VS ${message.author.tag}`)
+          .setDescription(
+            `The one with the ${
+              gamedata.type == "high" ? "**highest**" : "**lowest**"
+            } rate wins!`
+          )
+          .addFields({
+            name: `${gamedata.players.one.tag}`, value: `Rate: ${gamedata.players.oneR}`, inline: true
+          }, {
+            name: `${gamedata.players.two.tag}`, value: `Rate: ${gamedata.players.twoR}`, inline: true
+          }).setTimestamp();
+
+          mainMessage.edit({
+            embeds: [embed],
+            content: '_ _'
+          })
+
+          let winner = null;
+
+          if(gamedata.type == 'high') {
+            if(gamedata.players.oneR > gamedata.players.twoR){
+                winner = gamedata.players.one
+            } else if (gamedata.players.oneR < gamedata.players.twoR) {
+                winner = gamedata.players.two
+            } else {
+                winner = null
+            }
+          } else {
+            if(gamedata.players.oneR < gamedata.players.twoR){
+                winner = gamedata.players.one
+            } else if (gamedata.players.oneR > gamedata.players.twoR) {
+                winner = gamedata.players.two
+            } else {
+                winner = null
+            }
+          }
+
+          return message.channel.send(`:trophy: | ${winner ? winner.toString() : "Noone"} won the game!`)
+
+      } else {
+        confirmationMessage.edit({
+          embeds: [null],
+          content: "This request was DENIED.",
+          components: [null],
+        });
+        return;
+      }
+    });
+  },
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function getRate() {
+  return Math.floor(Math.random() * 100);
+}
+
+/**
+ */
+//stfu
