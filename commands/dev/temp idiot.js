9e5b029a39a2374e051267885e72e3084bcef9b0
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2023-03-22 21:35:02 +0530

    cs2 is real

---
+++ commands/dev/temp idiot.js
@@ -0,0 +1,14 @@
+// const thirty = require("../../database/models/30k")
+// const main = require('../../node_modules/discord-messages/models/messages')
+
+// const all = await thirty.find()
+
+// for await (const oneUser of all) {
+//     const temp = await main.findOne({
+//         userId: oneUser,
+//     })
+//     if(!temp || !temp?.messages) continue;
+//     temp.messages += oneUser.amount
+//     temp.save()
+
+// }
