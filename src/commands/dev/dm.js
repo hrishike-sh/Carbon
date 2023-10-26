683c734ba71aa6ace2f2a2fe41bbfae6f0b1e3d8
Ani <fellintron@gmail.com>
2023-10-26 18:50:27 +0530

    dumb

--- src/commands/dev/dm.js
+++ src/commands/dev/dm.js
@@ -1,73 +1,74 @@
-const { MessageEmbed } = require('discord.js');
+  const { MessageEmbed } = require('discord.js');
 
-const fh = '824294231447044197';
+  const fh = '824294231447044197';
+  const prefix = 'fh';
+  
+  module.exports = {
+    name: 'dm',
 
-module.exports = {
-  name: 'dm',
+    async execute(message) {
 
-  async execute(message) {
-
-    if(message.guild.id !== fh) {
-      return message.reply("you can't run this command");
-    }
-
-    console.log(`DM command triggered by ${message.author.tag} in #${message.channel.name}`);
-
-    if(!message.member.roles.cache.has('1016728636365209631')) {
-      return message.reply("Only CMs+ L bozo");
-    }
-
-    const user = message.mentions.users.first();
-    if(!user) return message.reply("specify a user");
-
-    console.log(`DMing user: ${user.tag}`);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const content = args.slice(1).join(" ");
-
-    if(!content) {
-      return message.reply("what's the message?");
-    }
-
-    console.log(`DM content: ${content}`);
-
-    const anonymous = args.includes('-a');
-
-    try {
-
-      const embed = new MessageEmbed()
-        .setDescription(`Message: ${content}`)
-        .setTimestamp();
-
-      if(!anonymous) {
-        embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
+      if(message.guild.id !== fh) {
+        return message.reply("you can't run this command");
       }
 
-      console.log('Attempting to send DM...');
-      
-      await user.send({ content: `You have a new message!`, embeds: [embed] });
+      console.log(`DM command triggered by ${message.author.tag} in #${message.channel.name}`);
 
-      console.log('DM sent!');
+      if(!message.member.roles.cache.has('1016728636365209631')) {
+        return message.reply("Only CMs+ L bozo");
+      }
 
-      await message.reply("sent em");
+      const user = message.mentions.users.first();
+      if(!user) return message.reply("specify a user");
 
-    } catch(err) {
-      console.log('Error sending DM:');
-      console.error(err);
-      await message.reply("unable to dm");
-    }
-    const emoji = message.guild.emojis.cache.get('1167089368775802940');
+      console.log(`DMing user: ${user.tag}`);
 
-    if (emoji) {
+      const args = message.content.slice(prefix.length).trim().split(/ +/);
+      const content = args.slice(1).join(" ");
 
-   await message.react(emoji);
+      if(!content) {
+        return message.reply("what's the message?");
+      }
 
-  } else {
+      console.log(`DM content: ${content}`);
 
-  console.log('error reacting');
+      const anonymous = args.includes('-a');
 
-}
+      try {
+
+        const embed = new MessageEmbed()
+          .setDescription(`Message: ${content}`)
+          .setTimestamp();
+
+        if(!anonymous) {
+          embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
+        }
+
+        console.log('Attempting to send DM...');
+        
+        await user.send({ content: `You have a new message!`, embeds: [embed] });
+
+        console.log('DM sent!');
+
+        await message.reply("sent em");
+
+      } catch(err) {
+        console.log('Error sending DM:');
+        console.error(err);
+        await message.reply("unable to dm");
+      }
+      const emoji = message.guild.emojis.cache.get('1167089368775802940');
+
+      if (emoji) {
+
+    await message.react(emoji);
+
+    } else {
+
+    console.log('error reacting');
 
   }
 
-}
+    }
+
+  }
