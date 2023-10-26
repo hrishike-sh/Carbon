365bdb994211f91cdf334409efdca8489ffb12cf
Ani <fellintron@gmail.com>
2023-10-26 17:44:41 +0530

    fix mentions

--- src/commands/perks/afk.js
+++ src/commands/perks/afk.js
@@ -1,27 +1,36 @@
 const { Message, Client } = require('discord.js');
+
 const DATABASE = require('../../database/afk');
 
 module.exports = {
+
   name: 'afk',
+
   cooldown: 5,
+
   roles: [
     '824687430753189902',
-    '825283097830096908',
+    '825283097830096908', 
     '831998003958906940',
     '826196972167757875',
     '839803117646512128',
     '824348974449819658',
     '999911429421408346'
   ],
+
   async execute(message, args, client) {
+
     let dbUser = await DATABASE.findOne({
       userId: message.author.id
     });
+
     if (dbUser) {
-      return message.reply("You're already AFK!");
+      return message.reply("You're already AFK!"); 
     }
 
-    const reason = args.join(' ') || 'AFK';
+    let reason = args.join(' ') || 'AFK';
+    
+    reason = reason.replace(/<@&(\d+)>/g, ''); 
 
     const entry = new DATABASE({
       userId: message.author.id,
@@ -29,11 +38,15 @@ module.exports = {
       time: new Date().getTime(),
       dms: []
     });
+
     entry.save();
 
-    message.reply(`You are now AFK!\nReason: ${reason}`);
+    message.reply(\`You are now AFK!\\nReason: ${reason}\`);
+
     setTimeout(() => {
       client.db.afks.push(message.author.id);
     }, 5000);
+
   }
+
 };
