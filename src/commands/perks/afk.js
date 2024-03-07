36631a2c9036a8ccdc43be337fa52a71819d6452
dauntless7 <nicolai.8293@gmail.com>
2024-03-07 16:55:04 -0400

    fix @everyone vulnerability in `afk`

--- src/commands/perks/afk.js
+++ src/commands/perks/afk.js
@@ -3,14 +3,13 @@ const { Message, Client } = require('discord.js');
 const DATABASE = require('../../database/afk');
 
 module.exports = {
-
   name: 'afk',
 
   cooldown: 5,
 
   roles: [
     '824687430753189902',
-    '825283097830096908', 
+    '825283097830096908',
     '831998003958906940',
     '826196972167757875',
     '839803117646512128',
@@ -19,18 +18,17 @@ module.exports = {
   ],
 
   async execute(message, args, client) {
-
     let dbUser = await DATABASE.findOne({
       userId: message.author.id
     });
 
     if (dbUser) {
-      return message.reply("You're already AFK!"); 
+      return message.reply("You're already AFK!");
     }
 
     let reason = args.join(' ') || 'AFK';
-    
-    reason = reason.replace(/<@&(\d+)>/g, ''); 
+
+    reason = reason.replace(/(@(everyone|here|[!&]?[\d]+))/gi, '');
 
     const entry = new DATABASE({
       userId: message.author.id,
@@ -46,7 +44,5 @@ module.exports = {
     setTimeout(() => {
       client.db.afks.push(message.author.id);
     }, 5000);
-
   }
-
 };
