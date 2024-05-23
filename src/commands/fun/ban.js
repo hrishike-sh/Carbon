9d250e15ea8feb6982a8a0d10560aea16b525b7c
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-05-23 19:04:52 +0530

    a few tests

--- src/commands/fun/ban.js
+++ src/commands/fun/ban.js
@@ -10,6 +10,13 @@ module.exports = {
    * @param {Client} client
    */
   async execute(message, args, client) {
-    message.reply('TEST');
+    if (message.guildId !== '1243193733223022823') return;
+
+    const target = message.mentions?.members.first();
+    if (!target) return message.reply('You have to mention someone!');
+    if (target.roles.cache.hasAny('1243194015487365202', '1243193938685460500'))
+      return message.reply('You cannot ban them idiot.');
+
+    message.reply(`would've banned ${target.toString()}`);
   }
 };
