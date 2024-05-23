6abe4e67c030c59c2e599ac358e129e59a80b455
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-05-23 21:14:13 +0530

    carl

---
+++ src/events/carl-modlogs-reader.js
@@ -0,0 +1,43 @@
+const { Message, Embed, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'messageCreate',
+  /**
+   *
+   * @param {Message} message
+   */
+  async execute(message) {
+    if (message.author.id !== '235148962103951360') return;
+    const text = message.attachments?.first();
+    if (
+      !text?.contentType.includes('text/plain') ||
+      !message.content.includes('Exported ')
+    )
+      return;
+
+    const data = getData(message.attachments.first().toJSON());
+    const embed = new EmbedBuilder()
+      .setTitle(data[0].offender_id)
+      .setDescription('You can filter types of modlogs.')
+      .setTimestamp()
+      .setColor('NotQuiteBlack');
+    for (const d of data) {
+      embed.addFields({
+        name: `Case #${d.case_id}`,
+        value: `Moderator: <@${d.moderator_id}>\nType: WARN\nWhen: <t:${(
+          Number(d.timestamp) / 1000
+        ).toFixed(0)}:R>`
+      });
+    }
+
+    message.channel.send({
+      embeds: [embed]
+    });
+  }
+};
+
+function getData(t) {
+  t.text().then((a) => {
+    return JSON.parse(a);
+  });
+}
