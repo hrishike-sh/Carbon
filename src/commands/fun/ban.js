13ccdb0a232fc33b2f1da26577033faeef91e6af
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-05-23 19:45:47 +0530

    t1

--- src/commands/fun/ban.js
+++ src/commands/fun/ban.js
@@ -17,6 +17,45 @@ module.exports = {
     if (target.roles.cache.hasAny('1243194015487365202', '1243193938685460500'))
       return message.reply('You cannot ban them idiot.');
 
-    message.reply(`would've banned ${target.toString()}`);
+    const chance = Math.random();
+
+    if (chance < 0.05) {
+      // ban yourself
+
+      // message.member.ban()
+      message.reply(
+        [
+          `**${message.author.username}** was too weak to use the ban hammer and banned themselves :sob::sob::sob:`,
+          `nobody likes you, you're banned **${message.author.username}**`
+        ][Math.floor(Math.random() * 2)]
+      );
+    } else if (chance < 0.1) {
+      // delete message + ban
+
+      message.delete();
+      message.channel.send(
+        `Someone has stealthily banned ${target.toString()}... :shushing_face:`
+      );
+
+      message.reply();
+    } else if (chance > 0.1 && chance < 0.2) {
+      // muted for a minute
+
+      message.channel.send("you're muted (doesnt work yet)");
+    } else if (chance < 0.4) {
+      // command fails
+
+      message.reply(
+        [
+          `${target.user.username} is too fast for you... you FAILED!`,
+          `${target.user.username} escaped AND tea-bagged you... its time to end it.`,
+          `${target.user.username} laughed your ban hammer off... you FAILED!`
+        ][Math.floor(Math.random() * 3)]
+      );
+    } else {
+      // ban
+
+      message.reply('normal ban');
+    }
   }
 };
