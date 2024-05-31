862b860b27702dfbdf7a302feb40a00813ee00a5
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-05-31 23:18:20 +0530

    15 seconds

--- src/commands/currency/memory.js
+++ src/commands/currency/memory.js
@@ -57,7 +57,7 @@ module.exports = {
       .setTitle('Memory Game')
       .setColor(Colors.Yellow)
       .setFooter({
-        text: `Win amount: ${(amount * 2.5).toLocaleString()} Time: 10 seconds.`
+        text: `Win amount: ${(amount * 2.5).toLocaleString()} Time: 15 seconds.`
       })
       .setDescription(
         'The game will start in 2 seconds, click the emojis in correct order later on!'
@@ -111,7 +111,7 @@ module.exports = {
         } else return true;
       },
       max: 5,
-      time: 10_000
+      time: 15_000
     });
     let count = 0;
     collector.on('collect', async (button) => {
