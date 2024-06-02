5afb8ab10724e257f484991cbd0b8c7c24e615b4
Ani <fellintron@gmail.com>
2024-06-02 14:53:21 +0530

    fix

--- src/commands/dev/dm.js
+++ src/commands/dev/dm.js
@@ -1,53 +1,42 @@
-const {  EmbedBuilder } = require('discord.js');
-
+const { EmbedBuilder } = require('discord.js');
 const fh = '824294231447044197'; 
 const prefix = 'fh';
-
 module.exports = {
   name: 'dm', 
   async execute(message) {
-
     if(message.guild.id !== fh) {
       return message.reply("");
     }
-
     if(!message.member.roles.cache.has('1016728636365209631')) {
       return message.reply("Only CMs+ L bozo"); 
     }
-
     const user = message.mentions.users.first();
     if(!user) return message.reply("specify a user");
-
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const content = args.slice(1).join(" ");
-
+    const anonymous = args.includes('-a');
+    
+    const filterArgs = args.slice(1).filter(arg => arg !== '-a' && !arg.startsWith('<@'));
+    const content = filterArgs.join(" ");
+    
     if(!content) {
       return message.reply("what's the message?");
     }
-
-    const anonymous = args.includes('-a');
-
+    
     try {
-
       const embed = new EmbedBuilder()
         .setDescription(content)
         .setTimestamp();
-
       if(!anonymous) {
         embed.setAuthor({
           name: message.author.tag,
           iconURL: message.author.displayAvatarURL() 
         });
       }
-
       await user.send({embeds: [embed]});
       await message.reply("sent em");
-
     } catch(err) {
       console.error(err);
       await message.reply("unable to dm");
     }
-
   }
-
 }
