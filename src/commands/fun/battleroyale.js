e48002c7356240b0a357ca618da6b273b8a70716
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-06-04 23:10:29 +0530

    big test 1

--- src/commands/fun/battleroyale.js
+++ src/commands/fun/battleroyale.js
@@ -81,43 +81,110 @@ module.exports = {
         if (i < 5) {
           mainRow[0].addComponents(
             new ButtonBuilder()
-              .setLabel(data.joined[i].name)
+              .setLabel(`${data.joined[i].name} (100)`)
               .setStyle(ButtonStyle.Secondary)
               .setCustomId(`br_${data.joined[i].id}_1`)
+              .setDisabled()
           );
         } else if (i < 10) {
           mainRow[1].addComponents(
             new ButtonBuilder()
-              .setLabel(data.joined[i].name)
+              .setLabel(`${data.joined[i].name} (100)`)
               .setStyle(ButtonStyle.Secondary)
               .setCustomId(`br_${data.joined[i].id}_2`)
+              .setDisabled()
           );
         } else if (i < 15) {
           mainRow[2].addComponents(
             new ButtonBuilder()
-              .setLabel(data.joined[i].name)
+              .setLabel(`${data.joined[i].name} (100)`)
               .setStyle(ButtonStyle.Secondary)
               .setCustomId(`br_${data.joined[i].id}_3`)
+              .setDisabled()
           );
         } else if (i < 20) {
           mainRow[3].addComponents(
             new ButtonBuilder()
-              .setLabel(data.joined[i].name)
+              .setLabel(`${data.joined[i].name} (100)`)
               .setStyle(ButtonStyle.Secondary)
               .setCustomId(`br_${data.joined[i].id}_4`)
+              .setDisabled()
           );
         } else {
           mainRow[4].addComponents(
             new ButtonBuilder()
-              .setLabel(data.joined[i].name)
+              .setLabel(`${data.joined[i].name} (100)`)
               .setStyle(ButtonStyle.Secondary)
               .setCustomId(`br_${data.joined[i].id}_5`)
+              .setDisabled()
           );
         }
       }
-      return message.reply({
+      const game_embed = new EmbedBuilder()
+        .setTitle('Battle Royale')
+        .setColor(Colors.Gold)
+        .setFooter('Last man standing wins!')
+        .setDescription(
+          `The game starts in **5 seconds**. Click on the button to attack that particular user.\n\nYour goal is to damage others and be the last one standing, good luck!`
+        );
+      const gameMessage = await message.channel.send({
         components: mainRow,
-        content: 'Test'
+        embeds: [game_embed]
+      });
+      await sleep(5000);
+      mainRow.forEach((c) => {
+        c.components.forEach((r) => {
+          r.setDisabled(false).setStyle(ButtonStyle.Primary);
+        });
+      });
+
+      const mainCollector = (
+        await gameMessage.edit({ components: mainRow })
+      ).createMessageComponentCollector({
+        filter: (m) => {
+          if (!data.joined.map((a) => a.id).includes(m.id)) {
+            m.reply({
+              ephemeral: true,
+              content: "You're not in this game!"
+            });
+            return false;
+          }
+          if (data.joined.find((a) => a.id == m.id).health < 1) {
+            m.reply({
+              ephemeral: true,
+              content: "You're already dead!"
+            });
+            return false;
+          }
+          return true;
+        }
+      });
+
+      mainCollector.on('collect', async (m) => {
+        const victim = data.joined.find(
+          (a) => a.id == m.customId.split('_')[1]
+        );
+        if (victim.id == m.user.id) {
+          m.reply({
+            ephemeral: true,
+            content: "You can't attack yourself!"
+          });
+          return;
+        }
+
+        const dmg = Math.ceil(Math.random() * 10) + 5;
+        victim.health -= dmg;
+        if (victim.health < 1) {
+          victim.health = 0;
+          mainRow[Number(m.customId.split('_')[2]) - 1].components
+            .find((a) => a.data.custom_id.includes(victim.id))
+            .setDisabled();
+        }
+        m.reply({
+          ephemeral: true,
+          content: `You attacked ${victim.name} and dealt ${dmg} damage!`
+        });
+        gameMessage.edit({ components: mainRow });
       });
     });
   }
@@ -129,3 +196,9 @@ const breakArray = (array) => {
   }
   return chunks;
 };
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
