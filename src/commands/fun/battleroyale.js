217eb91ad05b95d38e0316a982a81fb14b6f2506
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-06-05 12:56:25 +0530

    upgrades

--- src/commands/fun/battleroyale.js
+++ src/commands/fun/battleroyale.js
@@ -61,7 +61,8 @@ module.exports = {
       data.joined.push({
         id: m.user.id,
         name: m.user.tag,
-        health: 100
+        health: 100,
+        weapon: false
       });
       await m.reply({
         ephemeral: true,
@@ -77,72 +78,36 @@ module.exports = {
       for (let i = 1; i < Math.ceil(data.joined.length / 5); i++) {
         mainRow.push(new ActionRowBuilder());
       }
-      for (let i = 0; i < data.joined.length; i++) {
-        if (i < 5) {
-          mainRow[0].addComponents(
-            new ButtonBuilder()
-              .setLabel(`${data.joined[i].name} (100)`)
-              .setStyle(ButtonStyle.Secondary)
-              .setCustomId(`br_${data.joined[i].id}_1`)
-              .setDisabled()
-          );
-        } else if (i < 10) {
-          mainRow[1].addComponents(
-            new ButtonBuilder()
-              .setLabel(`${data.joined[i].name} (100)`)
-              .setStyle(ButtonStyle.Secondary)
-              .setCustomId(`br_${data.joined[i].id}_2`)
-              .setDisabled()
-          );
-        } else if (i < 15) {
-          mainRow[2].addComponents(
-            new ButtonBuilder()
-              .setLabel(`${data.joined[i].name} (100)`)
-              .setStyle(ButtonStyle.Secondary)
-              .setCustomId(`br_${data.joined[i].id}_3`)
-              .setDisabled()
-          );
-        } else if (i < 20) {
-          mainRow[3].addComponents(
-            new ButtonBuilder()
-              .setLabel(`${data.joined[i].name} (100)`)
-              .setStyle(ButtonStyle.Secondary)
-              .setCustomId(`br_${data.joined[i].id}_4`)
-              .setDisabled()
-          );
-        } else {
-          mainRow[4].addComponents(
-            new ButtonBuilder()
-              .setLabel(`${data.joined[i].name} (100)`)
-              .setStyle(ButtonStyle.Secondary)
-              .setCustomId(`br_${data.joined[i].id}_5`)
-              .setDisabled()
-          );
-        }
-      }
-      const game_embed = new EmbedBuilder()
-        .setTitle('Battle Royale')
-        .setColor(Colors.Gold)
-        .setFooter({
-          text: 'Last man standing wins!'
-        })
+
+      const upgradesEmbed = new EmbedBuilder()
+        .setTitle('Select your upgrades!')
+        .setColor('Green')
+        .setFooter('Game starts in 5 seconds.')
         .setDescription(
-          `The game starts in **5 seconds**. Click on the button to attack that particular user.\n\nYour goal is to damage others and be the last one standing, good luck!`
+          `<:dot:931436867272998922> Players: ${data.joined
+            .map((a) => `<@${a.id}>`)
+            .join(
+              ' '
+            )}\n\n<:yes:931435927061020712> **Weapon**: __+10__ Attack Damage\n<:yes:931435927061020712> **Shield**: __+50__ Health`
         );
-      const gameMessage = await message.channel.send({
-        components: mainRow,
-        embeds: [game_embed]
-      });
-      await sleep(5000);
-      mainRow.forEach((c) => {
-        c.components.forEach((r) => {
-          r.setDisabled(false).setStyle(ButtonStyle.Primary);
-        });
+      const upgradesRow = new ActionRowBuilder().addComponents([
+        new ButtonBuilder()
+          .setLabel('Weapon')
+          .setStyle(ButtonStyle.Success)
+          .setEmoji('🗡')
+          .setCustomId('br_up_wp'),
+        new ButtonBuilder()
+          .setLabel('Shield')
+          .setStyle(ButtonStyle.Success)
+          .setEmoji('🛡')
+          .setCustomId('br_up_sh')
+      ]);
+      const upgradesMessage = await message.channel.send({
+        embeds: [upgradesEmbed],
+        components: [upgradesRow]
       });
 
-      const mainCollector = (
-        await gameMessage.edit({ components: mainRow })
-      ).createMessageComponentCollector({
+      const upgradeCollector = upgradesMessage.createMessageComponentCollector({
         filter: (m) => {
           if (!data.joined.map((a) => a.id).includes(m.user.id)) {
             m.reply({
@@ -150,42 +115,150 @@ module.exports = {
               content: "You're not in this game!"
             });
             return false;
-          }
-          if (data.joined.find((a) => a.id == m.user.id).health < 1) {
+          } else if (
+            data.joined.map((a) => a.id).weapon ||
+            data.joined.map((a) => a.id).health > 100
+          ) {
             m.reply({
               ephemeral: true,
-              content: "You're already dead!"
+              content: "You can't upgrade more than once!"
             });
             return false;
-          }
-          return true;
+          } else return true;
+        },
+        time: 5_000
+      });
+      upgradeCollector.on('collect', async (button) => {
+        if (button.customId.includes('sh')) {
+          data.joined.find((a) => a.id == button.user.id).health += 50;
+          button.reply({
+            ephemeral: true,
+            content:
+              'You have upgraded your shield! You now have **150** Health!'
+          });
+        } else {
+          data.joined.find((a) => a.id == button.user.id).weapon = true;
+          button.reply({
+            ephemeral: true,
+            content:
+              'You have upgraded your weapon! You now deal **+10** damage!'
+          });
         }
       });
-
-      mainCollector.on('collect', async (m) => {
-        const victim = data.joined.find(
-          (a) => a.id == m.customId.split('_')[1]
-        );
-        if (victim.id == m.user.id) {
-          m.reply({
-            ephemeral: true,
-            content: "You can't attack yourself!"
+      upgradeCollector.on('end', async () => {
+        for (let i = 0; i < data.joined.length; i++) {
+          if (i < 5) {
+            mainRow[0].addComponents(
+              new ButtonBuilder()
+                .setLabel(`${data.joined[i].name} (100)`)
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId(`br_${data.joined[i].id}_1`)
+                .setDisabled()
+            );
+          } else if (i < 10) {
+            mainRow[1].addComponents(
+              new ButtonBuilder()
+                .setLabel(`${data.joined[i].name} (100)`)
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId(`br_${data.joined[i].id}_2`)
+                .setDisabled()
+            );
+          } else if (i < 15) {
+            mainRow[2].addComponents(
+              new ButtonBuilder()
+                .setLabel(`${data.joined[i].name} (100)`)
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId(`br_${data.joined[i].id}_3`)
+                .setDisabled()
+            );
+          } else if (i < 20) {
+            mainRow[3].addComponents(
+              new ButtonBuilder()
+                .setLabel(`${data.joined[i].name} (100)`)
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId(`br_${data.joined[i].id}_4`)
+                .setDisabled()
+            );
+          } else {
+            mainRow[4].addComponents(
+              new ButtonBuilder()
+                .setLabel(`${data.joined[i].name} (100)`)
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId(`br_${data.joined[i].id}_5`)
+                .setDisabled()
+            );
+          }
+        }
+        const game_embed = new EmbedBuilder()
+          .setTitle('Battle Royale')
+          .setColor(Colors.Gold)
+          .setFooter({
+            text: 'Last man standing wins!'
+          })
+          .setDescription(
+            `The game starts in **5 seconds**. Click on the button to attack that particular user.\n\nYour goal is to damage others and be the last one standing, good luck!`
+          );
+        const gameMessage = await message.channel.send({
+          components: mainRow,
+          embeds: [game_embed]
+        });
+        await sleep(5000);
+        mainRow.forEach((c) => {
+          c.components.forEach((r) => {
+            r.setDisabled(false).setStyle(ButtonStyle.Primary);
           });
-          return;
-        }
+        });
 
-        const dmg = Math.ceil(Math.random() * 10) + 5;
-        victim.health -= dmg;
-        const vBut = mainRow[
-          Number(m.customId.split('_')[2]) - 1
-        ].components.find((a) => a.data.custom_id.includes(victim.id));
-        if (victim.health < 1) {
-          victim.health = 0;
-          vBut.setDisabled().setEmoji('☠').setStyle(ButtonStyle.Secondary);
-        }
-        vBut.setLabel(`${victim.name} (${victim.health})`);
-        m.deferUpdate();
-        updateMessage(gameMessage, mainRow); // .
+        const mainCollector = (
+          await gameMessage.edit({ components: mainRow })
+        ).createMessageComponentCollector({
+          filter: (m) => {
+            if (!data.joined.map((a) => a.id).includes(m.user.id)) {
+              m.reply({
+                ephemeral: true,
+                content: "You're not in this game!"
+              });
+              return false;
+            }
+            if (data.joined.find((a) => a.id == m.user.id).health < 1) {
+              m.reply({
+                ephemeral: true,
+                content: "You're already dead!"
+              });
+              return false;
+            }
+            return true;
+          }
+        });
+
+        mainCollector.on('collect', async (m) => {
+          const victim = data.joined.find(
+            (a) => a.id == m.customId.split('_')[1]
+          );
+          if (victim.id == m.user.id) {
+            m.reply({
+              ephemeral: true,
+              content: "You can't attack yourself!"
+            });
+            return;
+          }
+
+          let dmg = Math.ceil(Math.random() * 10) + 5;
+          if (data.joined.find((a) => a.id == m.user.id).weapon) {
+            dmg += 10;
+          }
+          victim.health -= dmg;
+          const vBut = mainRow[
+            Number(m.customId.split('_')[2]) - 1
+          ].components.find((a) => a.data.custom_id.includes(victim.id));
+          if (victim.health < 1) {
+            victim.health = 0;
+            vBut.setDisabled().setEmoji('☠').setStyle(ButtonStyle.Secondary);
+          }
+          vBut.setLabel(`${victim.name} (${victim.health})`);
+          m.deferUpdate();
+          updateMessage(gameMessage, mainRow); // .
+        });
       });
     });
   }
