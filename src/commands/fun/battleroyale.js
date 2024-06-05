ac39b3f553893adc8027a8753f87a0c49ff09124
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-06-05 13:35:17 +0530

    uh

--- src/commands/fun/battleroyale.js
+++ src/commands/fun/battleroyale.js
@@ -330,8 +330,8 @@ let LASTUPDATE = new Date().getTime();
 function updateMessage(msg, components) {
   if (LASTUPDATE + 1000 < new Date().getTime()) {
     LASTUPDATE = new Date().getTime();
-    let row = components.components.sort(() => Math.random() - 0.5);
-    row.components.forEach((c) => {
+    let row = components.sort(() => Math.random() - 0.5);
+    row.forEach((c) => {
       c.components.sort(() => Math.random() - 0.5);
     });
     msg.edit({ components });
