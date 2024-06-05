cd7be6c493edc061f096ac6f094aa0ea0860c3ea
HrishikeshS123 <rushisalokhe456@gmail.com>
2024-06-06 01:39:26 +0530

    hm

--- src/commands/currency/memory.js
+++ src/commands/currency/memory.js
@@ -163,7 +163,7 @@ module.exports = {
     collector.on('end', async () => {
       end = true;
       console.log(count);
-      if (count == 5) {
+      if (count == 4) {
         await addCoins(userId, amount * 2.5);
         message.channel.send(
           `${message.author.toString()} you won <:token:1003272629286883450> **${(
