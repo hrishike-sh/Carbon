a184b184492da0adf30ef9514a1157f90d05a6eb
hrishike-sh <rushisalokhe456@gmail.com>
2024-06-27 00:18:40 +0530

    shop

--- src/commands/currency/shop.js
+++ src/commands/currency/shop.js
@@ -6,6 +6,7 @@ const {
   ActionRowBuilder,
   Colors
 } = require('discord.js');
+const Database = require('../../database/coins');
 
 const SHOP = [
   {
@@ -37,6 +38,16 @@ const SHOP = [
       id: '1003345268751741099'
     },
     value: 'role'
+  },
+  {
+    name: 'Absolutely Nothing',
+    price: 1000,
+    duration: Infinity,
+    emoji: {
+      str: '<:shrug:1255590797416333364>',
+      id: '1255590797416333364'
+    },
+    value: 'nothing'
   }
 ];
 module.exports = {
@@ -54,7 +65,7 @@ module.exports = {
       .setColor(Colors.Green)
       .addFields(
         SHOP.map((item) => ({
-          name: `${item.emoji.str} ${item.name}`,
+          name: `${item.emoji.str}${item.name}`,
           value: `<:blank:914473340129906708>**Price**: <:token:1003272629286883450> ${item.price.toLocaleString()}\n<:blank:914473340129906708>**Duration**: ${item.duration.toLocaleString()} Days`
         }))
       );
@@ -69,12 +80,56 @@ module.exports = {
             .setEmoji(item.emoji.str)
             .setDescription(`Price: ${item.price.toLocaleString()} coins.`);
         })
-      );
+      )
+      .setMaxValues(1)
+      .setMinValues(1);
     const row = new ActionRowBuilder().addComponents(selectMenu);
 
-    return message.reply({
+    const msg = await message.reply({
       embeds: [embed],
       components: [row]
+    });
+    const collector = msg.createMessageComponentCollector({
+      filter: (i) => i.user.id === message.author.id,
+      idle: 30_000,
+      max: 1
+    });
+    collector.on('collect', async (interaction) => {
+      const value = interaction.values[0];
+      interaction.deferUpdate();
+
+      if (value === 'nothing') {
+        const DatabaseUser = await Database.findOne({
+          userId: message.author.id
+        });
+        const item = SHOP.filter((a) => a.value == value)[0];
+        if (item.price > DatabaseUser?.coins) {
+          return msg.reply({
+            content: `${message.author} you don't have enough coins.`
+          });
+        }
+        DatabaseUser.coins -= item.price;
+        await DatabaseUser.save();
+        msg.reply({
+          embeds: [
+            {
+              color: Colors.Green,
+              description: `You have purchased ${
+                item.name
+              }!\n\n-${item.price.toLocaleString()}coins\n+Nothing`
+            }
+          ]
+        });
+      } else {
+        msg.reply({
+          content: 'You cannot buy this item yet.'
+        });
+      }
+    });
+
+    collector.on('end', (collected) => {
+      row.components[0].setDisabled(true);
+      msg.edit({ components: [row] });
     });
   }
 };
