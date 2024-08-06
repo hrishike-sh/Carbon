7c47055013aef77af06e160bd908d74cb43f5fc7
Hrishikesh <rushisalokhe456@gmail.com>
2024-08-06 12:41:03 +0530

    pass the token to the sharding manager

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env.token });
 require('dotenv').config();
-
-console.log(process.env.token);
+const manager = new ShardingManager('./bot.js', {
+  token: process.env.token
+});
 
 manager.on('shardCreate', (shard) =>
   console.log(`Connected shard: ${shard.id}`)
