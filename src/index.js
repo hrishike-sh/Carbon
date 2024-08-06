e7a36ef2f02fc0e850fff2049bed6d7bce4792d0
Hrishikesh <rushisalokhe456@gmail.com>
2024-08-06 12:36:42 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
-
 const manager = new ShardingManager('./bot.js', { token: process.env.token });
+require('dotenv').config();
 
 manager.on('shardCreate', (shard) =>
   console.log(`Connected shard: ${shard.id}`)
