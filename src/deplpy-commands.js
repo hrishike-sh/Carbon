dbda7095b0660bf7b5fa12a48161dd80175dd9b4
Hrishikesh <rushisalokhe456@gmail.com>
2024-08-07 18:23:25 +0530

    ok

--- src/deplpy-commands.js
+++ src/deplpy-commands.js
@@ -5,7 +5,7 @@ const path = require('node:path');
 
 const commands = [];
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, 'slash-commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
