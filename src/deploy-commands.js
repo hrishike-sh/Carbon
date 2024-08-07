b15dfb0a1b04c0cb7f2deaf71534c57a2c9eee78
Hrishikesh <rushisalokhe456@gmail.com>
2024-08-07 18:24:41 +0530

    ok

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -27,7 +27,7 @@ for (const folder of commandFolders) {
   }
 }
 
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(process.env.token);
 
 (async () => {
   try {
