8a258d312f5ad4776b46c535fc97dce21ea00d0e
Hrishikesh <rushisalokhe456@gmail.com>
2024-08-12 17:26:02 +0530

    ok

--- src/slash-commands/mods/ping-role.js
+++ src/slash-commands/mods/ping-role.js
@@ -64,6 +64,12 @@ module.exports = {
         .setName('event-type')
         .setDescription('Type of the event. (rumble, bo3 kick etc.)')
         .setRequired(false);
+    })
+    .addBooleanOption((b) => {
+      return b
+        .setName('event')
+        .setDescription('Is it an event?')
+        .setRequired(false);
     }),
   /**
    *
@@ -93,18 +99,19 @@ module.exports = {
       prize: interaction.options.get('prize'),
       requirement: interaction.options.get('requirement'),
       message: interaction.options.get('message'),
-      eventType: interaction.options.get('event-type')
+      eventType: interaction.options.get('event-type'),
+      event: interaction.options.get('event')
     };
 
     if (data.role.value == '824916330574118942') {
       // giveaway ping
 
-      // if (interaction.channel.id !== '826065190973210634') {
-      //   return interaction.reply({
-      //     ephemeral: true,
-      //     content: 'You can only run this command in <#826065190973210634>'
-      //   });
-      // }
+      if (interaction.channel.id !== '826065190973210634') {
+        return interaction.reply({
+          ephemeral: true,
+          content: 'You can only run this command in <#826065190973210634>'
+        });
+      }
       if (interaction.user.id != '598918643727990784')
         return interaction.reply('Testing only!');
       if (cooldowns.get('giveaway')) {
@@ -124,10 +131,70 @@ module.exports = {
         content: 'Pinged!',
         ephemeral: true
       });
+
+      await interaction.channel.send({
+        content: `<@&${
+          data.role.value
+        }>: Make sure to thank ${data.sponsor.user.toString()} in <#870240187198885888>!`
+      });
     } else if (data.role.value == '858088201451995137') {
       // event ping
     } else if (data.role.value == '837121985787592704') {
       // mini gaw and event ping
+
+      if (
+        interaction.channel.id !== '826065190973210634' ||
+        interaction.channel.id != '853280287777882142'
+      ) {
+        return interaction.reply({
+          ephemeral: true,
+          content:
+            'You can only run this command in <#826065190973210634> and <#853280287777882142>'
+        });
+      }
+      if (cooldowns.get('mgaw')) {
+        const date = cooldowns.get('mgaw');
+        if (Date.now() < date) {
+          return interaction.reply({
+            ephemeral: true,
+            content: `This command is on cooldown! Try again <t:${Math.floor(
+              date / 1000
+            )}:R>`
+          });
+        }
+      }
+      cooldowns.set('mgaw', Date.now() + 1800000);
+
+      interaction.reply({
+        content: 'Pinged!',
+        ephemeral: true
+      });
+
+      if (data.event.value) {
+        const embed = new EmbedBuilder()
+          .setTitle('Event')
+          .setColor('#9BFA8D')
+          .setDescription(
+            `\n⦿ **Event**: ${
+              data.eventType.value
+            }\n⦿ **Sponsor:** ${data.sponsor.member.toString()}\n⦿ **Requirement:** ${
+              data.requirement.value || 'No requirement'
+            }⦿ **Prize:** ${data.prize.value}\n`
+          );
+        if (data.message) {
+          embed.setDescription(embed.data.description + '⦿ **Prize:**');
+        }
+
+        return interaction.channel.send({
+          embeds: [embed]
+        });
+      } else {
+        await interaction.channel.send({
+          content: `<@&${
+            data.role.value
+          }>: Make sure to thank ${data.sponsor.user.toString()} in <#870240187198885888>!`
+        });
+      }
     } else if (data.role.value == '1154432845318721607') {
       // mafia ping
     }
