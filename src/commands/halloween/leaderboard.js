d196b82b922ca2f931092b1445c3f27604d84d8f
hrishike-sh <rushisalokhe456@gmail.com>
2024-10-24 19:20:03 +0530

    a

---
+++ src/commands/halloween/leaderboard.js
@@ -0,0 +1,25 @@
+const { Message, Client, Colors, EmbedBuilder } = require('discord.js');
+const halloween = require('../../database/halloween');
+
+module.exports = {
+  name: 'halloween-leaderboard',
+  aliases: 'hlb',
+  /**
+   *
+   * @param {Message} message
+   * @param {String[]} args
+   * @param {Client} client
+   */
+  async execute(message, args, client) {
+    const raw = await halloween.find({});
+    const mapped = raw.map(
+      (a, ind) => `${ind + 1}. <@${a.roleId}>: ${a.points} points`
+    );
+    const embed = new EmbedBuilder()
+      .setTitle(':ghost: Halloween Leaderboard')
+      .setColor(Colors.Purple)
+      .setDescription(mapped.join('\n'));
+
+    return message.reply({ embeds: [embed] });
+  }
+};
