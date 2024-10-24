730919c2dc78aa728ca9be1a0dbbc224dc43cd21
hrishike-sh <rushisalokhe456@gmail.com>
2024-10-24 19:21:50 +0530

    leaderboard final

--- src/commands/halloween/leaderboard.js
+++ src/commands/halloween/leaderboard.js
@@ -11,14 +11,15 @@ module.exports = {
    * @param {Client} client
    */
   async execute(message, args, client) {
-    const raw = await halloween.find({});
+    const raw = await halloween.find({}).sort({ points: -1 }).limit(10);
     const mapped = raw.map(
-      (a, ind) => `${ind + 1}. <@${a.roleId}>: ${a.points} points`
+      (a, ind) => `${ind + 1}. <@&${a.roleId}>: ${a.points} points`
     );
     const embed = new EmbedBuilder()
       .setTitle(':ghost: Halloween Leaderboard')
       .setColor(Colors.Purple)
-      .setDescription(mapped.join('\n'));
+      .setDescription(mapped.join('\n'))
+      .setTimestamp();
 
     return message.reply({ embeds: [embed] });
   }
