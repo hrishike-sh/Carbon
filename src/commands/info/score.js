cfc5e942f3790176345b10a79c59b4806f2419f2
Hrishikesh <rushisalokhe456@gmail.com>
2025-03-31 22:00:55 +0530

    a

--- src/commands/info/score.js
+++ src/commands/info/score.js
@@ -11,6 +11,7 @@ module.exports = {
    * @param {String[]} args
    */
   async execute(message, args) {
+    console.log('Command executed: score');
     const msg = await message.channel.send({
       embeds: [
         {
@@ -20,14 +21,17 @@ module.exports = {
     });
 
     try {
+      console.log('Fetching data from API...');
       const req = await fetch(
         `https://api.cricapi.com/v1/currentMatches?apikey=${API_KEY}&offset=0`
       );
       if (!req.ok) {
+        console.error('Error fetching data:', req.status);
         throw new Error(req.status);
       }
 
       const data = await req.json();
+      console.log('Data fetched successfully');
       const match = data.data[0];
       const embed = new EmbedBuilder().setTitle(match.title).setColor(0x00ffff);
       for (let i = 0; i < 2; i++) {
@@ -38,6 +42,9 @@ module.exports = {
       }
 
       await msg.edit({ embeds: [embed] });
-    } catch (error) {}
+      console.log('Message edited with match data');
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
   }
 };
