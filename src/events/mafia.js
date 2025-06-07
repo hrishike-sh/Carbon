5743b641ff41e2dd343c275e534f618c4c23712a
Hrishikesh <rushisalokhe456@gmail.com>
2025-06-07 20:21:04 +0530

    ok

--- src/events/mafia.js
+++ src/events/mafia.js
@@ -85,7 +85,7 @@ module.exports = {
           const d = new Collection();
           for (const msg of currentGame.messages) {
             console.log(msg.night, nightNumber);
-            if (msg.night == nightNumber) {
+            if (msg.night == nightNumber - 1) {
               if (d.has(msg.user)) {
                 d.set(msg.user, d.get(msg.user) + 1);
               } else {
