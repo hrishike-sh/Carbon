5240d183068d02539216e7220f7ba7c5727afd27
Hrishikesh <rushisalokhe456@gmail.com>
2025-06-15 10:05:33 +0530

    crab race

--- src/commands/2025/events.js
+++ src/commands/2025/events.js
@@ -368,6 +368,166 @@ module.exports = {
           ]
         });
       });
+    } else if (index == 3) {
+      const joinEmbed = new EmbedBuilder()
+        .setTitle('Crab Race :crab:')
+        .setDescription(
+          `Click the button to join the **Crab Race**!\n\nGame starts in **30 seconds**`
+        )
+        .setColor('Yellow');
+      const joinButton = new ButtonBuilder()
+        .setLabel('Join')
+        .setCustomId('join;tr')
+        .setStyle(ButtonStyle.Success);
+      const joinRow = new ActionRowBuilder().addComponents([joinButton]);
+      const joinMessage = await message.channel.send({
+        embeds: [joinEmbed],
+        components: [joinRow]
+      });
+      const joinCollector = joinMessage.createMessageComponentCollector({
+        time: 30_000
+      });
+
+      const gamedata = {
+        joined: [],
+        tracks: []
+      };
+
+      joinCollector.on('collect', async (button) => {
+        if (gamedata.joined.includes(button.user.id)) {
+          return button.reply({
+            content: 'You have already joined the game',
+            ephemeral: true
+          });
+        }
+        gamedata.joined.push(button.user.id);
+        if (gamedata.joined.length == 10) {
+          joinCollector.stop();
+        }
+
+        button.reply({
+          content: 'You have joined the game!',
+          ephemeral: true
+        });
+      });
+      joinCollector.on('end', async () => {
+        joinButton.setDisabled();
+        joinMessage.edit({
+          components: [joinRow]
+        });
+
+        if (gamedata.joined.length < 2) {
+          return message.reply('You need atleast 2 players to play this game.');
+        }
+
+        for (const userId of gamedata.joined) {
+          gamedata.tracks.push({
+            user: client.users.cache.get(userId),
+            track: [
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●',
+              '●'
+            ]
+          });
+        }
+
+        let description = gamedata.tracks
+          .map(
+            (track) =>
+              `**${track.user.tag}**:\n:squid:${track.track.join(' ')} :crab:`
+          )
+          .join('\n');
+
+        const mainMessage = await message.channel.send({
+          embeds: [
+            {
+              description,
+              title: 'Crab Race',
+              color: Colors.Green,
+              timestamp: new Date()
+            }
+          ]
+        });
+        let end = false;
+        for (let i = 0; i < 50; i++) {
+          if (end == true) continue;
+          for (const track of gamedata.tracks) {
+            console.log(track);
+            const rand = Math.ceil(Math.random() * 10);
+            let blocks = 0;
+            if (rand < 6) blocks = 2;
+            if (rand > 5 && rand < 10) blocks = 3;
+            if (rand > 9) blocks = 5;
+
+            if (track.length < blocks) {
+              blocks = track.length;
+              end = true;
+              track.track.splice(0, blocks);
+            } else {
+              track.track.splice(0, blocks);
+            }
+          }
+          await sleep(2500);
+          description = gamedata.tracks
+            .map(
+              (track) =>
+                `**${track.user.tag}**:\n${
+                  track.track.length < 1 ? ':crown:' : ':squid:'
+                }${track.track.join(' ')} :crab:`
+            )
+            .join('\n');
+          mainMessage.edit({
+            embeds: [
+              {
+                description,
+                title: 'Crab Race',
+                color: Colors.Green,
+                timestamp: new Date()
+              }
+            ]
+          });
+          if (description.includes(':crown:')) {
+            const winner = gamedata.tracks.find((t) => t.track.length == 0);
+
+            mainMessage.reply(`${winner.user.toString()} has won! :tada:`);
+            break;
+          }
+        }
+      });
     }
   }
 };
