c44e2078d5b679b459b01f2584787416d6b04bb8
hrishike-sh <rushisalokhe456@gmail.com>
2025-12-18 18:37:30 +0530

    whoops

--- src/commands/halloween/leaderboard.js
+++ src/commands/halloween/leaderboard.js
@@ -1,26 +1,26 @@
-const { Message, Client, Colors, EmbedBuilder } = require('discord.js');
-const halloween = require('../../database/halloween');
+// const { Message, Client, Colors, EmbedBuilder } = require('discord.js');
+// const halloween = require('../../database/halloween');
 
-module.exports = {
-  name: 'halloween-leaderboard',
-  aliases: 'hlb',
-  /**
-   *
-   * @param {Message} message
-   * @param {String[]} args
-   * @param {Client} client
-   */
-  async execute(message, args, client) {
-    const raw = await halloween.find({}).sort({ points: -1 }).limit(10);
-    const mapped = raw.map(
-      (a, ind) => `${ind + 1}. <@&${a.roleId}>: ${a.points} points`
-    );
-    const embed = new EmbedBuilder()
-      .setTitle(':ghost: Halloween Leaderboard')
-      .setColor(Colors.Purple)
-      .setDescription(mapped.join('\n'))
-      .setTimestamp();
+// module.exports = {
+//   name: 'halloween-leaderboard',
+//   aliases: 'hlb',
+//   /**
+//    *
+//    * @param {Message} message
+//    * @param {String[]} args
+//    * @param {Client} client
+//    */
+//   async execute(message, args, client) {
+//     const raw = await halloween.find({}).sort({ points: -1 }).limit(10);
+//     const mapped = raw.map(
+//       (a, ind) => `${ind + 1}. <@&${a.roleId}>: ${a.points} points`
+//     );
+//     const embed = new EmbedBuilder()
+//       .setTitle(':ghost: Halloween Leaderboard')
+//       .setColor(Colors.Purple)
+//       .setDescription(mapped.join('\n'))
+//       .setTimestamp();
 
-    return message.reply({ embeds: [embed] });
-  }
-};
+//     return message.reply({ embeds: [embed] });
+//   }
+// };
