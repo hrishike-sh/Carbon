33a9e2779b82fdaf7a1c102a68ae2aedfc068bd8
Hrishikesh <rushisalokhe456@gmail.com>
2026-06-28 00:03:06 +0530

    Hardcode AFK command role IDs

--- src/commands/perks/afk.js
+++ src/commands/perks/afk.js
@@ -1,14 +1,16 @@
-const config = require('../../config');
 const DATABASE = require('../../database/models/afk');
 
 module.exports = {
   name: 'afk',
   cooldown: 5,
   roles: [
-    config.roles.staff.mod,
-    config.roles.staff.admin,
-    config.roles.staff.cman,
-    config.roles.giveawayManager
+    '824687430753189902',
+    '825283097830096908',
+    '831998003958906940',
+    '826196972167757875',
+    '839803117646512128',
+    '824348974449819658',
+    '999911429421408346'
   ],
 
   async execute(message, args, client) {
